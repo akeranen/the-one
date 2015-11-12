a95dec0a8e3b44a5b471f564b5a1fd8e1409d24d
teemuk <teemuk@spacetimenetworks.com>
2015-11-12 13:36:39 +0100

    Added core.Constants to hold global constants. Currently only holds a DEBUG flag.

---
+++ core/Constants.java
@@ -0,0 +1,14 @@
+package core;
+
+/**
+ * Global constants.
+ *
+ * @author teemuk
+ */
+public final class Constants {
+	/** Disallow instantiation */
+	private Constants() {}
+
+	/** Enable/disable debug mode. Used to compile out debug logging etc. */
+	public static final boolean DEBUG = false;
+}

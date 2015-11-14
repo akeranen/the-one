3e1e875ad4b747b9cf0bbabb2c9956aae9710c3c
Ari Keränen <ari.keranen@gmail.com>
2015-11-14 18:59:56 +0200

    Moved GUI assets to src dir and copy them in compile scripts

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
 javac -sourcepath src -d target -extdirs lib/ src/core/*.java src/movement/*.java src/report/*.java src/routing/*.java src/gui/*.java src/input/*.java src/applications/*.java src/interfaces/*.java
 
-mkdir target/gui/buttonGraphics
-copy src/gui/buttonGraphics/* target/gui/buttonGraphics/
+cp -R src/gui/buttonGraphics target/gui/

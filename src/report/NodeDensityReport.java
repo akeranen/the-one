96d8f0f630e97383bb5a8273e51a6a6b7b1c1201
teemuk <teemuk@spacetimenetworks.com>
2015-11-17 17:10:24 +0100

    Added gnuplot output option to NodeDensityReport.

--- src/report/NodeDensityReport.java
+++ src/report/NodeDensityReport.java
@@ -26,11 +26,15 @@ extends SamplingReport {
 	public static final String X_COUNT_SETTING = "xCount";
 	/** Number of divisions along the y-axis ({@value}). */
 	public static final String Y_COUNT_SETTING = "yCount";
+	/** Boolean setting to output gnuplot file ({@value}). */
+	public static final String GNUPLOT_SETTING = "outputGnuplot";
 
 	/** Default number of divisions along the x-axis ({@value}). */
 	public static final int DEFAULT_X_COUNT = 10;
 	/** Default number of divisions along the y-axis ({@value}). */
 	public static final int DEFAULT_Y_COUNT = 10;
+	/** Default value for outputting gnuplot instead of raw data ({@value}). */
+	public static final boolean DEFAULT_GNUPLOT_SETTING = false;
 	//========================================================================//
 
 
@@ -41,6 +45,8 @@ extends SamplingReport {
 	private final int verticalCount;
 	private final double divisionWidth;
 	private final double divisionHeight;
+	private final boolean gnuplot;
+	private final String runName;
 	private final List<int[][]> samples;
 	//========================================================================//
 
@@ -51,8 +57,12 @@ extends SamplingReport {
 	public NodeDensityReport() {
 		super();
 
-		// Set up the sampling grid
 		final Settings settings = super.getSettings();
+
+		this.gnuplot = settings.getBoolean(GNUPLOT_SETTING,
+				DEFAULT_GNUPLOT_SETTING);
+
+		// Set up the sampling grid
 		this.horizontalCount
 				= settings.getInt(X_COUNT_SETTING, DEFAULT_X_COUNT);
 		this.verticalCount
@@ -75,6 +85,8 @@ extends SamplingReport {
 		final int sampleCount = (int) (duration / super.interval + 1);
 
 		this.samples = new ArrayList<>(sampleCount);
+
+		this.runName = scenario.getName();
 	}
 	//========================================================================//
 
@@ -105,9 +117,17 @@ extends SamplingReport {
 	@Override
 	public void done() {
 
+		if (this.gnuplot) {
+			this.outputGnuplotPrefix();
+		}
+
+		final double[][] averages = getAverageDensities(this.samples,
+				this.horizontalCount, this.verticalCount);
+
 		for (int g_x = 0; g_x < this.horizontalCount; g_x++) {
 			for (int g_y = 0; g_y < this.verticalCount; g_y++) {
-				String line = "" + g_x + " " + g_y;
+				String line = "" + g_x + " " + g_y + " "
+						+ averages[g_x][g_y];
 				for (final int[][] sample : this.samples) {
 					line += " " + sample[g_x][g_y];
 				}
@@ -115,7 +135,64 @@ extends SamplingReport {
 			}
 		}
 
+		if (this.gnuplot) {
+			this.outputGnuplotSuffix(this.samples.size());
+		}
+
 		super.done();
+	}
+	//========================================================================//
+
+
+	//========================================================================//
+	// Private
+	//========================================================================//
+	private static double[][] getAverageDensities(
+			final List<int[][]> samples,
+			final int horizontalCount,
+			final int verticalCount) {
+		final double[][] averages = new double[horizontalCount][verticalCount];
+
+		// Get total count
+		for (final int[][] sample : samples) {
+			for (int g_x = 0; g_x < horizontalCount; g_x++) {
+				for (int g_y = 0; g_y < verticalCount; g_y++) {
+					averages[g_x][g_y] += sample[g_x][g_y];
+				}
+			}
+		}
+
+		// Calculate average
+		final int count = samples.size();
+		for (int g_x = 0; g_x < horizontalCount; g_x++) {
+			for (int g_y = 0; g_y < verticalCount; g_y++) {
+				averages[g_x][g_y] /= count;
+			}
+		}
+
+		return averages;
+	}
+	//========================================================================//
+
+
+	//========================================================================//
+	// Private - Gnuplot
+	//========================================================================//
+	private void outputGnuplotPrefix() {
+		super.write("set terminal png");
+		super.write("$data << EOD");
+	}
+
+	private void outputGnuplotSuffix(final int sampleCount) {
+		super.write("EOD");
+		super.write("set output sprintf('" + this.runName
+				+ "-avrg.png')");
+		super.write("plot '$data' using 2:1:3 with image");
+		super.write("do for [ii=4:" + (3 + sampleCount) + "] {");
+		super.write("set output sprintf('" + this.runName
+						+ "%04.0f.png',ii-3)");
+		super.write("plot '$data' using 2:1:ii with image");
+		super.write("}");
 	}
 	//========================================================================//
 }

1be3246489e4a28681e119391d0ad1d108bb195a
teemuk <teemuk@spacetimenetworks.com>
2016-11-21 10:27:04 +0100

    Expanded javadocs and tweaked the output of RadiusOfGyrationReport.

--- src/report/RadiusOfGyrationReport.java
+++ src/report/RadiusOfGyrationReport.java
@@ -7,7 +7,23 @@ import core.SimScenario;
 import java.util.List;
 
 /**
- * Calculates the radius of gyration for all the nodes in the simulation.
+ * <p>Calculates the radius of gyration for all the nodes in the simulation. The
+ * radius of gyration is calculated by sampling the simulation at fixed
+ * intervals (defined by the '{@link SamplingReport#SAMPLE_INTERVAL_SETTING}'
+ * setting) to record the position of all the nodes. For each node, the
+ * center of the mass of the trajectory is calculated from the samples. The
+ * radius of gyration is the standard deviation of the distance of the
+ * samples from the center of the mass of the trajectory.
+ * </p>
+ *
+ * <p>In other words, for each node:
+ * r_g = sqrt( 1/n * sum_i( distance( r_i, r_cm )^2 ) ),
+ * where r_g is the radius of gyration, n is the number of samples, r_i is the
+ * coordinate of the sample i of the trajectory, r_cm is the coordinate of
+ * the center of mass of the samples:
+ * r_cm = 1/n * sum_i( r_i ).</p>
+ *
+ * <p>The output line format is: "node_id node_name r_cm_x r_cm_y r_g"</p>
  *
  * @author teemuk
  */
@@ -62,7 +78,7 @@ extends SamplingReport {
 			}
 
 			final double rg = Math.sqrt(1.0 / sampleCount * d_squared);
-			super.write(this.nodeNames[i] + " "
+			super.write("" + i + " " + this.nodeNames[i] + " "
 					+ geomCenter[0] + " " + geomCenter[1] + " "
 					+ rg);
 		}

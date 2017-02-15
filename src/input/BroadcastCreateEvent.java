a99c3f333afce20a8a67787298b7dbabc5bb8b65
Britta Heymann <britta_hey@web.de>
2017-02-15 11:21:35 +0100

    Remove unused type parameter in MessageEvent
    refs #40

--- src/input/BroadcastCreateEvent.java
+++ src/input/BroadcastCreateEvent.java
@@ -2,7 +2,6 @@ package input;
 
 import core.BroadcastMessage;
 import core.DTNHost;
-import core.Message;
 import core.World;
 
 /**
@@ -24,7 +23,7 @@ public class BroadcastCreateEvent extends MessageEvent {
      * @param time Time, when the message is created
      */
     public BroadcastCreateEvent(int from, String id, int size, int responseSize, double time) {
-        super(from, -1, id, time, Message.MessageType.BROADCAST);
+        super(from, -1, id, time);
         this.size = size;
         this.responseSize = responseSize;
     }
--- src/input/MessageEvent.java
+++ src/input/MessageEvent.java
@@ -16,19 +16,6 @@ public abstract class MessageEvent extends ExternalEvent {
 	protected int toAddr;
 	/** identifier of the message */
 	protected String id;
-	/** message type **/
-	protected Message.MessageType type;
-
-	/**
-	 * Creates a message  event
-	 * @param from Where the message comes from
-	 * @param to Who the message goes to
-	 * @param id ID of the message
-	 * @param time Time when the message event occurs
-	 */
-	public MessageEvent(int from, int to, String id, double time) {
-        this(from, to, id, time, Message.MessageType.ONE_TO_ONE);
-	}
 
 	/**
 	 * Creates a message event.
@@ -36,14 +23,12 @@ public abstract class MessageEvent extends ExternalEvent {
 	 * @param to Who the message goes to
 	 * @param id ID of the message
 	 * @param time Time when the message event occurs
-	 * @param type Type of the message
 	 */
-	public MessageEvent(int from, int to, String id, double time, Message.MessageType type) {
+	public MessageEvent(int from, int to, String id, double time) {
 		super(time);
 		this.fromAddr = from;
 		this.toAddr= to;
 		this.id = id;
-		this.type = type;
 	}
 
 	@Override

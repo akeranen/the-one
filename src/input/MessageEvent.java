0564e41955939bf047d3af7e40b6e3af7d30ae3b
Britta Heymann <britta_hey@web.de>
2017-02-15 09:40:15 +0100

    Support broadcast creation via external event

--- src/input/MessageEvent.java
+++ src/input/MessageEvent.java
@@ -8,12 +8,19 @@ package input;
  * A message related external event
  */
 public abstract class MessageEvent extends ExternalEvent {
+    public enum MessageType {
+        ONE_TO_ONE,
+        BROADCAST
+	}
+
 	/** address of the node the message is from */
 	protected int fromAddr;
 	/** address of the node the message is to */
 	protected int toAddr;
 	/** identifier of the message */
 	protected String id;
+	/** message type **/
+	protected MessageType type;
 
 	/**
 	 * Creates a message  event
@@ -23,10 +30,23 @@ public abstract class MessageEvent extends ExternalEvent {
 	 * @param time Time when the message event occurs
 	 */
 	public MessageEvent(int from, int to, String id, double time) {
+        this(from, to, id, time, MessageType.ONE_TO_ONE);
+	}
+
+	/**
+	 * Creates a message event.
+	 * @param from Where the message comes from
+	 * @param to Who the message goes to
+	 * @param id ID of the message
+	 * @param time Time when the message event occurs
+	 * @param type Type of the message
+	 */
+	public MessageEvent(int from, int to, String id, double time, MessageType type) {
 		super(time);
 		this.fromAddr = from;
 		this.toAddr= to;
 		this.id = id;
+		this.type = type;
 	}
 
 	@Override

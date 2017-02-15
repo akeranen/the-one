a7d724d261bfb2e08ca3f8f69459c7f3cf7d4c74
Marius Meyer <marius-meyer@hotmail.com>
2017-02-15 17:26:50 +0100

    Init new event types for MM

---
+++ src/input/VHMEvent.java
@@ -0,0 +1,78 @@
+package input;
+
+import core.Coord;
+
+/**
+ * Created by Marius Meyer on 15.02.17.
+ */
+public class VHMEvent extends ExternalEvent {
+
+    private static final int MIN_INTENSITY = 1;
+    private static final int MAX_INTENSITY = 10;
+
+    /**
+     * Type of a VHMEvent. This event type can be requested by a node
+     */
+    public enum VHMEventType{
+        DISASTER,
+        HOSPITAL
+    }
+
+    public VHMEventType getType() {
+        return type;
+    }
+
+    public double getStartTime() {
+        return startTime;
+    }
+
+    public double getEndTime() {
+        return endTime;
+    }
+
+    public Coord getLocation() {
+        return location;
+    }
+
+    public double getEventRange() {
+        return eventRange;
+    }
+
+    public double getSafeRange() {
+        return safeRange;
+    }
+
+    public double getMaxRange() {
+        return maxRange;
+    }
+
+    public int getIntensity() {
+        return intensity;
+    }
+
+    private VHMEventType type;
+    private double startTime;
+    private double endTime;
+    private Coord location;
+    private double eventRange;
+    private double safeRange;
+    private double maxRange;
+    private int intensity;
+
+    public VHMEvent(VHMEventType type, double startTime, double endTime, Coord location, double eventRange,
+                    double safeRange, double maxRange,int intensity){
+        super(startTime);
+        assert type != null : "Type must be set!";
+        this.type = type;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.location = location;
+        this.eventRange = eventRange;
+        this.safeRange = safeRange;
+        this.maxRange = maxRange;
+        assert intensity >= MIN_INTENSITY && intensity <= MAX_INTENSITY : "Intensity must be integer between " + MIN_INTENSITY
+                + " and " + MAX_INTENSITY;
+        this.intensity = intensity;
+    }
+
+}
---
+++ src/input/VHMEventGenerator.java
@@ -0,0 +1,17 @@
+package input;
+
+/**
+ * Created by Marius Meyer on 15.02.17.
+ */
+public class VHMEventGenerator implements EventQueue {
+
+    @Override
+    public ExternalEvent nextEvent() {
+        return null;
+    }
+
+    @Override
+    public double nextEventsTime() {
+        return 0;
+    }
+}

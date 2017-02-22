6d2c724a362eb599c3cc2cba67e10eea13fd288d
Marcus <marcusn@mail.uni-paderborn.de>
2017-02-22 11:07:58 +0100

    Revert "Update test"
    This reverts commit af3436510a9705664fb8a3e8382a707d2ad67d26.

--- src/movement/TransportingMovement.java
+++ src/movement/TransportingMovement.java
@@ -5,23 +5,23 @@ import core.Settings;
 import movement.map.MapNode;
 
 public class TransportingMovement extends ShortestPathMapBasedMovement{
-
+	
     /** the destination of the transport */
     private MapNode transportDestination;
     /** the location of the event */
     private MapNode eventLocation;
-
+	
     /**
      * Creates a new TransportingMovement model based on a Settings object's settings.
      * @param settings The Settings object where the settings are read from
      */
     public TransportingMovement(Settings settings){
         super(settings);
-        //to choose locations, specify pois in settings
+		//to choose locations, specify pois in settings
         this.transportDestination = pois.selectDestination();
         this.eventLocation = pois.selectDestination();
     }
-
+	
     /**
      * Copyconstructor.
      * @param tm The TransportingMovement prototype to base the new object to
@@ -31,23 +31,23 @@ public class TransportingMovement extends ShortestPathMapBasedMovement{
         this.transportDestination = tm.transportDestination;
         this.eventLocation = tm.eventLocation;
     }
-
+	
     @Override
     public Path getPath(){
         Path path = new Path(generateSpeed());
         MapNode destination = selectDestination();
-
+		 
         List<MapNode> nodePath = pathFinder.getShortestPath(lastMapNode, destination);
-
+		 
         for (MapNode node : nodePath) {
             path.addWaypoint(node.getLocation());
-        }
-
+		}
+		 
         lastMapNode = destination;
-
+		 
         return path;
-    }
-
+	}
+	
     /**
      * Chooses the next destination depending on the current location. If the host is not
      * at the eventLocation yet, it moves there first to start the transport.
@@ -58,7 +58,7 @@ public class TransportingMovement extends ShortestPathMapBasedMovement{
     public MapNode selectDestination(){
         MapNode destination;
         if(host.getLocation().compareTo(eventLocation.getLocation()) != 0 && 
-                host.getLocation().compareTo(transportDestination.getLocation()) != 0){
+				host.getLocation().compareTo(transportDestination.getLocation()) != 0){
             destination = eventLocation;
         } else if(host.getLocation().compareTo(eventLocation.getLocation()) == 0){
             destination = transportDestination;
@@ -68,25 +68,25 @@ public class TransportingMovement extends ShortestPathMapBasedMovement{
         }
         return destination;
     }
-
+	
     /**
      * Gets the eventLocation of this TransportingMovement.
      * @return The eventLocation
      */
     public MapNode getEventLocation(){
         return this.eventLocation;
-    }
-
+	}
+	
     /**
      * Gets the transportDestination of this TransportingMovement.
      * @return The transportDestination
      */
     public MapNode getTransportDestination(){
         return this.transportDestination;
-    }
-
+	}
+	
     @Override
     public TransportingMovement replicate(){
         return new TransportingMovement(this);
-    }
+	}
 }

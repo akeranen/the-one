fd4128a0146e98a5836c8f15f9bfbbc9701faffd
Marcus <marcusn@mail.uni-paderborn.de>
2017-02-22 09:19:24 +0100

    Resolve all sonar issues and add some docu

--- src/movement/TransportingMovement.java
+++ src/movement/TransportingMovement.java
@@ -6,72 +6,87 @@ import movement.map.MapNode;
 
 public class TransportingMovement extends ShortestPathMapBasedMovement{
 	
-	/** the destination of the transport */
-	private MapNode transportDestination;
-	/** the location of the event */
-	private MapNode eventLocation;
+    /** the destination of the transport */
+    private MapNode transportDestination;
+    /** the location of the event */
+    private MapNode eventLocation;
 	
-	/**
-	 * Creates a new TransportingMovement model based on a Settings object's settings.
-	 * @param settings The Settings object where the settings are read from
-	 */
-	public TransportingMovement(Settings settings){
-		super(settings);
+    /**
+     * Creates a new TransportingMovement model based on a Settings object's settings.
+     * @param settings The Settings object where the settings are read from
+     */
+    public TransportingMovement(Settings settings){
+        super(settings);
 		//to choose locations, specify pois in settings
-		this.transportDestination = pois.selectDestination();
-		this.eventLocation = pois.selectDestination();
-	}
+        this.transportDestination = pois.selectDestination();
+        this.eventLocation = pois.selectDestination();
+    }
 	
-	/**
-	 * Copyconstructor.
-	 * @param tm The TransportingMovement prototype to base the new object to
-	 */
-	protected TransportingMovement(TransportingMovement tm){
-		super(tm);
-		this.transportDestination = tm.transportDestination;
-		this.eventLocation = tm.eventLocation;
-	}
+    /**
+     * Copyconstructor.
+     * @param tm The TransportingMovement prototype to base the new object to
+     */
+    protected TransportingMovement(TransportingMovement tm){
+        super(tm);
+        this.transportDestination = tm.transportDestination;
+        this.eventLocation = tm.eventLocation;
+    }
 	
-	@Override
-	public Path getPath(){
-		Path path = new Path(generateSpeed());
-		MapNode destination = selectDestination();
-		
-		List<MapNode> nodePath = pathFinder.getShortestPath(lastMapNode, destination);
-
-		for (MapNode node : nodePath) {
-			path.addWaypoint(node.getLocation());
+    @Override
+    public Path getPath(){
+        Path path = new Path(generateSpeed());
+        MapNode destination = selectDestination();
+		 
+        List<MapNode> nodePath = pathFinder.getShortestPath(lastMapNode, destination);
+		 
+        for (MapNode node : nodePath) {
+            path.addWaypoint(node.getLocation());
 		}
-		
-		lastMapNode = destination;
-		
-		return path;
+		 
+        lastMapNode = destination;
+		 
+        return path;
 	}
 	
-	public MapNode selectDestination(){
-		MapNode destination;
-		if(host.getLocation().compareTo(eventLocation.getLocation()) != 0 && 
+    /**
+     * Chooses the next destination depending on the current location. If the host is not
+     * at the eventLocation yet, it moves there first to start the transport.
+     * Stays at the same location, if host already is at the transportDestination and
+     * prepares to change its movement model.
+     * @return The next destination the host moves to
+     */
+    public MapNode selectDestination(){
+        MapNode destination;
+        if(host.getLocation().compareTo(eventLocation.getLocation()) != 0 && 
 				host.getLocation().compareTo(transportDestination.getLocation()) != 0){
-			destination = eventLocation;
-		} else if(host.getLocation().compareTo(eventLocation.getLocation()) == 0){
+            destination = eventLocation;
+        } else if(host.getLocation().compareTo(eventLocation.getLocation()) == 0){
             destination = transportDestination;
-		} else {
+        } else {
             isReady();
-				destination = transportDestination;
-			}
-		return destination;
+            destination = transportDestination;
+        }
+        return destination;
+    }
+	
+    /**
+     * Gets the eventLocation of this TransportingMovement.
+     * @return The eventLocation
+     */
+    public MapNode getEventLocation(){
+        return this.eventLocation;
 	}
 	
-	public MapNode getEventLocation(){
-		return this.eventLocation;
+    /**
+     * Gets the transportDestination of this TransportingMovement.
+     * @return The transportDestination
+     */
+    public MapNode getTransportDestination(){
+        return this.transportDestination;
 	}
 	
-	public MapNode getTransportDestination(){
-		return this.transportDestination;
-	}
-	
-	@Override
-	public TransportingMovement replicate(){
-		return new TransportingMovement(this);
+    @Override
+    public TransportingMovement replicate(){
+        return new TransportingMovement(this);
 	}
 }

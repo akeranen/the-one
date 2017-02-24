812edf44d349836bf00f93f11bf73401ed8191fa
Melanie Bruns <mbruns42@mail.upb.de>
2017-02-24 10:10:21 +0100

    Add AbstractMovementModelTest
    Add a superclass that covers the basic functionalities of MovementModels and make LevyWalk and
    MapBasedMovement inherit from it. However, ExternalMovement does not inherit since it behaves
    differently.

--- src/test/LevyWalkTest.java
+++ src/test/LevyWalkTest.java
@@ -1,10 +1,9 @@
 package test;
 
 import core.Coord;
-import core.Settings;
 import movement.LevyWalkMovement;
+import movement.MovementModel;
 import movement.Path;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -15,58 +14,23 @@ import static org.junit.Assert.assertNotNull;
 
 /**
  *
- * Test for Levy Walk movement model.
+ * Tests for Levy Walk movement model.
  *
  * @author MelanieBruns
  */
-public class LevyWalkTest{
+public class LevyWalkTest extends AbstractMovementModelTest{
 
     private LevyWalkMovement levy = new LevyWalkMovement(new TestSettings());
-    private static final int MAX_COORD = 1000;
     private static final int TEST_RUNS = 2000;
     private static final int TEST_RADIUS = 100;
     private static final int CENTER_COORD = 500;
     private static final int EDGE_COORD = 50;
 
-
-    @Before
-    public void setUp(){
-        Settings.init(null);
-        java.util.Locale.setDefault(java.util.Locale.US);
-        TestSettings testSettings = new TestSettings();
-        levy = new LevyWalkMovement(testSettings);
-        levy.getInitialLocation();
+    public MovementModel initializeModel(TestSettings settings){
+        levy = new LevyWalkMovement(settings);
+        return levy;
     }
 
-    @Test
-    public void testGetPath(){
-        Path path = levy.getPath();
-        assertNotNull(path);
-        List<Coord> coords = path.getCoords();
-        //Get the first point on the path
-        Coord nextWaypoint = coords.get(1);
-        assertNotNull(nextWaypoint);
-        //Bounds in test Settings are 1000x1000
-        assertTrue( nextWaypoint.getX()>=0);
-        assertTrue( nextWaypoint.getY()>=0);
-        assertTrue( nextWaypoint.getX()<=MAX_COORD);
-        assertTrue( nextWaypoint.getY()<=MAX_COORD);
-    }
-
-    @Test
-    public void testStaysWithinDefaultBounds(){
-        for (int i=1; i<TEST_RUNS; i++){
-            Path p = levy.getPath();
-            List<Coord> coords = p.getCoords();
-            Coord nextWaypoint = coords.get(1);
-            assertNotNull(nextWaypoint);
-            //Bounds in test Settings are 1000x1000
-            assertTrue( nextWaypoint.getX()>=0);
-            assertTrue( nextWaypoint.getY()>=0);
-            assertTrue( nextWaypoint.getX()<=MAX_COORD);
-            assertTrue( nextWaypoint.getY()<=MAX_COORD);
-        }
-    }
 
     @Test
     public void testStaysWithinRadius(){
@@ -79,9 +43,10 @@ public class LevyWalkTest{
             Path p = levy.getPath();
             List<Coord> coords = p.getCoords();
             Coord nextWaypoint = coords.get(1);
-            assertNotNull(nextWaypoint);
+            assertNotNull("Path did not include a valid next waypoint.", nextWaypoint);
             //Bounds in test Settings are 1000x1000
-            assertTrue(center.distance(nextWaypoint)<=TEST_RADIUS);
+            assertTrue("Path was outside the allowed radius.", center.distance(nextWaypoint)<=TEST_RADIUS);
+
         }
     }
 
@@ -97,13 +62,13 @@ public class LevyWalkTest{
             Path p = levy.getPath();
             List<Coord> coords = p.getCoords();
             Coord nextWaypoint = coords.get(1);
-            assertNotNull(nextWaypoint);
+            assertNotNull("Path did not include a valid next waypoint.",nextWaypoint);
             //Bounds in test Settings are 1000x1000
-            assertTrue(center.distance(nextWaypoint)<=TEST_RADIUS);
-            assertTrue( nextWaypoint.getX()>=0);
-            assertTrue( nextWaypoint.getY()>=0);
-            assertTrue( nextWaypoint.getX()<=MAX_COORD);
-            assertTrue( nextWaypoint.getY()<=MAX_COORD);
+            assertTrue("Path was outside the radius.",center.distance(nextWaypoint)<=TEST_RADIUS);
+            assertTrue(MESSAGE_X_BELOW_ZERO, nextWaypoint.getX()>=0);
+            assertTrue(MESSAGE_Y_BELOW_ZERO,nextWaypoint.getY()>=0);
+            assertTrue(MESSAGE_X_ABOVE_LIMIT,nextWaypoint.getX()<=MAX_COORD);
+            assertTrue(MESSAGE_Y_ABOVE_LIMIT, nextWaypoint.getY()<=MAX_COORD);
         }
     }
 
--- src/test/MapBasedMovementTest.java
+++ src/test/MapBasedMovementTest.java
@@ -12,7 +12,6 @@ import java.io.PrintWriter;
 import java.io.StringReader;
 import java.util.List;
 
-import junit.framework.TestCase;
 import movement.MapBasedMovement;
 import movement.MovementModel;
 import movement.Path;
@@ -21,8 +20,14 @@ import movement.map.SimMap;
 import core.Coord;
 import core.DTNHost;
 import core.Settings;
+import org.junit.Test;
 
-public class MapBasedMovementTest extends TestCase {
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+
+public class MapBasedMovementTest extends AbstractMovementModelTest {
 	/* Topology:  n7--n5
 	 *            |   |
 	 *        n1--n2--n6--n3
@@ -47,15 +52,16 @@ public class MapBasedMovementTest extends TestCase {
 	private SimMap map;
 	private TestSettings s;
 
+
 	@Override
-	public void setUp() throws Exception {
-		super.setUp();
-		java.util.Locale.setDefault(java.util.Locale.US);
-		s = new TestSettings();
+	public MovementModel initializeModel(TestSettings testSettings) {
+		setupMapData(null, null, null);
+		return mbm;
 	}
 
 	private void setupMapData(String okTypes, String speed, String wTime) {
 		Settings.init(null);
+		s = new TestSettings();
 		StringReader input = new StringReader(WKT);
 
 		WKTMapReader reader = new WKTMapReader(true);
@@ -79,7 +85,8 @@ public class MapBasedMovementTest extends TestCase {
 		n6 = map.getNodeByCoord(c6);
 	}
 
-	public void testGetPath() {
+	@Test
+	public void testDistanceBetweenNodes() {
 		setupMapData(null,null,null);
 		Coord c;
 		mbm.getInitialLocation();
@@ -89,7 +96,7 @@ public class MapBasedMovementTest extends TestCase {
 		while (path.hasNext()) {
 			c2 = path.getNextWaypoint();
 			// adjacent nodes are always 1 meter apart (in the test topology)
-			assertEquals(1.0, c.distance(c2));
+			assertTrue(c.distance(c2)<=1 && c.distance(c2)>=1);
 			c = c2;
 		}
 	}

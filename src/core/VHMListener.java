86ab3988a790e5c19ee560c1b750e78c722eaa08
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-02-28 14:11:32 +0100

    Clarify javadoc for the VHMListener interface

--- src/core/VHMListener.java
+++ src/core/VHMListener.java
@@ -3,7 +3,8 @@ package core;
 import input.VHMEvent;
 
 /**
- * This is an Interface for all classes that want to listen to the disaster events of the VouluntaryHelperMobility model.
+ * This is an Interface for all classes that want to listen to the VHMEvents of the VouluntaryHelperMobility model.
+ * Those events can be disasters or hospitals.
  * Created by Ansgar Mährlein on 15.02.2017.
  */
 public interface VHMListener {

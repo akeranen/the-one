e5891addac53641c0d7ebff092ef4a26639754bc
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-03-24 15:45:07 +0100

    Fix SonarQube issues

--- src/input/VhmEvent.java
+++ src/input/VhmEvent.java
@@ -5,6 +5,7 @@ import core.SimError;
 
 import javax.json.JsonNumber;
 import javax.json.JsonObject;
+import javax.json.stream.JsonParsingException;
 import java.io.IOException;
 
 /**
@@ -143,9 +144,8 @@ public class VhmEvent extends ExternalEvent {
      *
      * @param name   The name of the event. This should be the key value of the JSON object
      * @param object The JSON object, that represents the event
-     * @throws IOException If the JSON object could not be parsed to an VhmEvent
      */
-    public VhmEvent(String name, JsonObject object) throws IOException {
+    public VhmEvent(String name, JsonObject object){
         super(0);
         try {
             this.id = getNextEventID();
@@ -185,8 +185,8 @@ public class VhmEvent extends ExternalEvent {
             } else {
                 this.intensity = DEFAULT_INTENSITY;
             }
-        } catch (Exception e) {
-            throw new IOException("VhmEvent could not be parsed from JSON: " + e);
+        } catch (JsonParsingException e) {
+            throw new SimError("VhmEvent could not be parsed from JSON: " + e);
         }
     }
 

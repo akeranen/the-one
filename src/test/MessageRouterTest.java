ddc9b143d84d7ab08817037139007a2622a1769e
Marius Meyer <mariusme@mail.upb.de>
2017-03-31 15:42:43 +0200

    Add tests for improvements in MessageRouter.java

--- src/test/MessageRouterTest.java
+++ src/test/MessageRouterTest.java
@@ -1,20 +1,23 @@
 package test;
 
-import core.*;
+import core.Application;
+import core.BroadcastMessage;
+import core.ConnectionListener;
+import core.DTNHost;
+import core.Group;
+import core.Message;
+import core.MessageListener;
+import core.MulticastMessage;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import routing.MessageRouter;
 import routing.PassiveRouter;
-
 import routing.util.RoutingInfo;
-
-import test.TestUtils;
-import test.TestSettings;
 
 import java.util.ArrayList;
 
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Contains tests for the message router class.
@@ -22,14 +25,24 @@ import static org.junit.Assert.*;
  * Created by Britta Heymann on 15.02.2017.
  */
 public class MessageRouterTest {
+
+    private static final int DEFAULT_MESSAGE_SIZE = 100;
+
     private TestUtils utils;
 
     private Message msg;
     private Message broadcast;
+    private Message multicast;
     private DTNHost recipient;
+    private DTNHost sender;
+
+    public MessageRouterTest(){
+        //set up is done in methods annotated with @Before
+    }
 
     @Before
     public void setUp() {
+        Group.clearGroups();
         this.utils = new TestUtils(
                 new ArrayList<ConnectionListener>(),
                 new ArrayList<MessageListener>(),
@@ -37,9 +50,13 @@ public class MessageRouterTest {
         // Use passive router as that is nearest to the original MessageRouter class
         this.utils.setMessageRouterProto(new PassiveRouter(new TestSettings()));
         this.recipient = this.utils.createHost();
+        this.sender = this.utils.createHost();
+        Group g = Group.createGroup(0);
+        g.addHost(sender);
 
-        this.msg = new Message(this.utils.createHost(), recipient, "M", 100);
-        this.broadcast = new BroadcastMessage(this.utils.createHost(), "B", 50);
+        this.msg = new Message(sender, recipient, "M", DEFAULT_MESSAGE_SIZE);
+        this.broadcast = new BroadcastMessage(sender, "B", DEFAULT_MESSAGE_SIZE);
+        this.multicast = new MulticastMessage(sender, g,"G",DEFAULT_MESSAGE_SIZE);
     }
 
     @Test
@@ -47,8 +64,8 @@ public class MessageRouterTest {
         DTNHost nonRecipient = this.utils.createHost();
         MessageRouter nonRecipientRouter = nonRecipient.getRouter();
 
-        this.checkInitialBufferIsEmpty(nonRecipientRouter);
-        this.checkNoMessagesHaveBeenDeliveredAfterInitialization(nonRecipientRouter);
+        checkInitialBufferIsEmpty(nonRecipientRouter);
+        checkNoMessagesHaveBeenDeliveredAfterInitialization(nonRecipientRouter);
         nonRecipient.receiveMessage(this.msg, this.msg.getFrom());
         nonRecipient.messageTransferred(this.msg.getId(), this.msg.getFrom());
 
@@ -57,7 +74,7 @@ public class MessageRouterTest {
         assertEquals(
                 "Message should not have been added to delivered messages",
                 0,
-                this.getNrOfDeliveredMessages(nonRecipientRouter));
+                getNrOfDeliveredMessages(nonRecipientRouter));
     }
 
     @Test
@@ -66,7 +83,7 @@ public class MessageRouterTest {
         MessageRouter nonRecipientRouter = nonRecipient.getRouter();
         nonRecipientRouter.addApplication(new DroppingApplication());
 
-        this.checkInitialBufferIsEmpty(nonRecipientRouter);
+        checkInitialBufferIsEmpty(nonRecipientRouter);
         nonRecipient.receiveMessage(this.msg, this.msg.getFrom());
         nonRecipient.messageTransferred(this.msg.getId(), this.msg.getFrom());
 
@@ -80,15 +97,15 @@ public class MessageRouterTest {
     public void testMessageTransferredForSingleRecipientPutsMessageIntoDeliveredButNotIntoBuffer() {
         MessageRouter recipientRouter = this.recipient.getRouter();
 
-        this.checkInitialBufferIsEmpty(recipientRouter);
-        this.checkNoMessagesHaveBeenDeliveredAfterInitialization(recipientRouter);
+        checkInitialBufferIsEmpty(recipientRouter);
+        checkNoMessagesHaveBeenDeliveredAfterInitialization(recipientRouter);
         this.recipient.receiveMessage(this.msg, this.msg.getFrom());
         this.recipient.messageTransferred(this.msg.getId(), this.msg.getFrom());
 
         assertEquals(
                 "Message should have been put into delivered messages.",
                 1,
-                this.getNrOfDeliveredMessages(recipientRouter));
+                getNrOfDeliveredMessages(recipientRouter));
         assertEquals(
                 "Message should not have been put into buffer.",
                 0,
@@ -99,7 +116,7 @@ public class MessageRouterTest {
     public void testMessageTransferredForSingleRecipientDoesNotPutMessageIntoDeliveredForSecondTime() {
         MessageRouter recipientRouter = this.recipient.getRouter();
 
-        this.checkNoMessagesHaveBeenDeliveredAfterInitialization(recipientRouter);
+        checkNoMessagesHaveBeenDeliveredAfterInitialization(recipientRouter);
         this.recipient.receiveMessage(this.msg, this.msg.getFrom());
         this.recipient.messageTransferred(this.msg.getId(), this.msg.getFrom());
         this.recipient.receiveMessage(this.msg, this.msg.getFrom());
@@ -108,7 +125,7 @@ public class MessageRouterTest {
         assertEquals(
                 "Message should not have been put into delivered messages twice.",
                 1,
-                this.getNrOfDeliveredMessages(recipientRouter));
+                getNrOfDeliveredMessages(recipientRouter));
     }
 
     @Test
@@ -116,8 +133,8 @@ public class MessageRouterTest {
         DTNHost host = this.utils.createHost();
         MessageRouter router = host.getRouter();
 
-        this.checkInitialBufferIsEmpty(router);
-        this.checkNoMessagesHaveBeenDeliveredAfterInitialization(router);
+        checkInitialBufferIsEmpty(router);
+        checkNoMessagesHaveBeenDeliveredAfterInitialization(router);
         host.receiveMessage(this.broadcast, this.broadcast.getFrom());
         host.messageTransferred(this.broadcast.getId(), this.broadcast.getFrom());
 
@@ -126,7 +143,7 @@ public class MessageRouterTest {
         assertEquals(
                 "Broadcast should have been added to delivered messages",
                 1,
-                this.getNrOfDeliveredMessages(router));
+                getNrOfDeliveredMessages(router));
     }
 
     @Test
@@ -134,7 +151,7 @@ public class MessageRouterTest {
         DTNHost host = this.utils.createHost();
         MessageRouter router = host.getRouter();
 
-        this.checkNoMessagesHaveBeenDeliveredAfterInitialization(router);
+        checkNoMessagesHaveBeenDeliveredAfterInitialization(router);
         host.receiveMessage(this.broadcast, this.broadcast.getFrom());
         host.messageTransferred(this.broadcast.getId(), this.broadcast.getFrom());
         host.receiveMessage(this.broadcast, this.broadcast.getFrom());
@@ -143,7 +160,7 @@ public class MessageRouterTest {
         assertEquals(
                 "Broadcast should not have been added to delivered messages twice.",
                 1,
-                this.getNrOfDeliveredMessages(router));
+                getNrOfDeliveredMessages(router));
     }
 
     @Test
@@ -152,7 +169,7 @@ public class MessageRouterTest {
         MessageRouter router = host.getRouter();
         router.addApplication(new DroppingApplication());
 
-        this.checkInitialBufferIsEmpty(router);
+        checkInitialBufferIsEmpty(router);
         router.receiveMessage(this.broadcast, this.broadcast.getFrom());
         router.messageTransferred(this.broadcast.getId(), this.broadcast.getFrom());
 
@@ -162,11 +179,37 @@ public class MessageRouterTest {
                 router.getNrofMessages());
     }
 
+    @Test
+    public void testSenderOfBroadcastRecognizesNoFirstDeliveryForOwnMessage(){
+        MessageRouter router = sender.getRouter();
+        sender.createNewMessage(this.broadcast);
+        Assert.assertTrue("Broadcast should be set as delivered message after creation",
+                router.isDeliveredMessage(this.broadcast));
+
+    }
+
+    @Test
+    public void testSenderOfMulticastRecognizesNoFirstDeliveryForOwnMessage(){
+        MessageRouter router = sender.getRouter();
+        sender.createNewMessage(this.multicast);
+        Assert.assertTrue("Multicast should be set as delivered message after creation",
+                router.isDeliveredMessage(this.multicast));
+    }
+
+    @Test
+    public void testSenderOfUnicastRecognizesFirstDeliveryForOwnMessage(){
+        MessageRouter router = sender.getRouter();
+        Message senderToSenderMsg = new Message(sender,sender,"S to S",DEFAULT_MESSAGE_SIZE);
+        sender.createNewMessage(senderToSenderMsg);
+        Assert.assertFalse("Unicast should not be set as delivered message after creation",
+                router.isDeliveredMessage(this.multicast));
+    }
+
     /**
      * Asserts that the router's buffer is empty.
      * @param router The MessageRouter to look at.
      */
-    private void checkInitialBufferIsEmpty(MessageRouter router) {
+    private static void checkInitialBufferIsEmpty(MessageRouter router) {
         assertEquals(
                 "Initial number of messages should have been different.",
                 0,
@@ -177,11 +220,11 @@ public class MessageRouterTest {
      * Asserts that the number of delivered messages to the router is 0.
      * @param router The MessageRouter to look at.
      */
-    private void checkNoMessagesHaveBeenDeliveredAfterInitialization(MessageRouter router) {
+    private static void checkNoMessagesHaveBeenDeliveredAfterInitialization(MessageRouter router) {
         assertEquals(
                 "Initial number of delivered messages should have been different.",
                 0,
-                this.getNrOfDeliveredMessages(router));
+                getNrOfDeliveredMessages(router));
     }
 
     /**
@@ -189,7 +232,7 @@ public class MessageRouterTest {
      * @param router The router to look at.
      * @return The number of delivered messages to the router.
      */
-    private int getNrOfDeliveredMessages(MessageRouter router) {
+    private static int getNrOfDeliveredMessages(MessageRouter router) {
         RoutingInfo info = router.getRoutingInfo();
         for(RoutingInfo additionalInfo : info.getMoreInfo()) {
             if(additionalInfo.toString().contains("delivered message(s)")) {
@@ -203,7 +246,7 @@ public class MessageRouterTest {
     /**
      * An application that simply drops every message.
      */
-    private class DroppingApplication extends Application {
+    private static class DroppingApplication extends Application {
         @Override
         public Message handle(Message msg, DTNHost host) {
             return null;
@@ -211,7 +254,7 @@ public class MessageRouterTest {
 
         @Override
         public void update(DTNHost host) {
-
+            //just a dummy application, so no functionality needed here
         }
 
         @Override

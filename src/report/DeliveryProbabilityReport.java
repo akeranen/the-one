0aef9478cfec9f5524b2435e40f9733bb0d244a0
Nils Weidmann <Nils Weidmann>
2017-04-05 14:30:38 +0200

    Minor corrections to the DeliveryProbabilityReport according to review suggestions

--- src/report/DeliveryProbabilityReport.java
+++ src/report/DeliveryProbabilityReport.java
@@ -16,134 +16,128 @@ import core.MessageListener;
  */
 public class DeliveryProbabilityReport  extends Report implements MessageListener {
 
-	private Map<String, Double> creationTimes;
-	private List<Double> latencies;
-	private List<Integer> hopCounts;
-	private List<Double> roundTripTimes;
+    private Map<String, Double> creationTimes;
+    private List<Double> latencies;
+    private List<Integer> hopCounts;
+    private List<Double> roundTripTimes;
 
-	private int nrofCreated;
-	private int nrofDelivered;
-	
-	/**
-	 * Constructor, it just calls the init-Methods
-	 */
-	public DeliveryProbabilityReport() {
-		init();
-	}
-	
-	/**
-	 * Sets basic settings
-	 */
-	@Override
-	protected void init() {
-		super.init();
-		this.creationTimes = new HashMap<String, Double>();
-		this.latencies = new ArrayList<Double>();
-		this.hopCounts = new ArrayList<Integer>();
-		this.roundTripTimes = new ArrayList<Double>();
+    private int nrofCreated;
+    private int nrofDelivered;
+    
+    /**
+     * Constructor, it just calls the init-Methods
+     */
+    public DeliveryProbabilityReport() {
+        init();
+    }
 
-		this.nrofCreated = 0;
-		this.nrofDelivered = 0;
-	}
-	
-	/**
-	 * Is called when a message is deleted from the buffer
-	 * Method is not used, but must be implemented due to the class hierarchy
-	 * @param m Message to be delivered
-	 * @param where DTN host where the Message is located
-	 * @param dropped
-	 */
-	public void messageDeleted(Message m, DTNHost where, boolean dropped) {
-		if (isWarmupID(m.getId())) {
-			return;
-		}
-	}
-	
-	/**
-	 * Method is not used, but must be implemented due to the class hierarchy
-	 * @param m Message to be delivered
-	 * @param from sender of the message
-	 * @param to receiver of the message
-	 */
-	public void messageTransferAborted(Message m, DTNHost from, DTNHost to) {
-		// Method is not used, but must be implemented due to the class hierarchy
-	}
-	
-	/**
-	 * @param m Message
-	 * @param from sender of the message
-	 * @param to receiver of the message
-	 * @param finalTarget intended target of the message
-	 */
-	public void messageTransferred(Message m, DTNHost from, DTNHost to, boolean finalTarget) {
-		if (isWarmupID(m.getId())) {
-			return;
-		}
-        
-		if (m.getType() == core.Message.MessageType.ONE_TO_ONE && finalTarget) {
-			this.latencies.add(getSimTime() -
-				this.creationTimes.get(m.getId()) );
-			this.nrofDelivered++;
-			this.hopCounts.add(m.getHops().size() - 1);
+    /**
+     * Sets basic settings
+     */
+    @Override
+    protected void init() {
+        super.init();
+        this.creationTimes = new HashMap<String, Double>();
+        this.latencies = new ArrayList<Double>();
+        this.hopCounts = new ArrayList<Integer>();
+        this.roundTripTimes = new ArrayList<Double>();
 
-			if (m.isResponse()) {
-				this.roundTripTimes.add(getSimTime() -	m.getRequest().getCreationTime());
-			}
-		}
-	}
-	
-	/**
-	 * This method is invoked when a new message is created
-	 * @param m Message that was recently created
-	 */
-	public void newMessage(Message m) {
-		if (isWarmup()) {
-			addWarmupID(m.getId());
-			return;
-		}
-
-		this.creationTimes.put(m.getId(), getSimTime());
-
-		switch (m.getType()) {
-            case ONE_TO_ONE:
-            	this.nrofCreated += 1;
-                break;
-            case BROADCAST:
-                return; //only one-to-one messages are considered
-            default:
-                throw new UnsupportedOperationException("No implementation for message type " + m.getType() + ".");
+        this.nrofCreated = 0;
+        this.nrofDelivered = 0;
+    }
+    
+    /**
+     * Is called when a message is deleted from the buffer
+     * Method is not used, but must be implemented due to the class hierarchy
+     * @param m Message to be delivered
+     * @param where DTN host where the Message is located
+     * @param dropped
+     */
+    public void messageDeleted(Message m, DTNHost where, boolean dropped) {
+        if (isWarmupID(m.getId())) {
+            return;
         }
-	}
-	
-	/**
-	 * Method is not used, but must be implemented due to the class hierarchy
-	 * @param m Message to be delivered
-	 * @param from sender of the message
-	 * @param to receiver of the message
-	 */
-	public void messageTransferStarted(Message m, DTNHost from, DTNHost to) {
-		// Method is not used, but must be implemented due to the class hierarchy
-	}
-	
-	/**
-	 * Writes the results of the report to a text file
-	 */
-	@Override
-	public void done() {
-		write("Message stats for scenario " + getScenarioName() +
-				"\nsim_time: " + format(getSimTime()));
-		double deliveryProb = 0;
-		
-		if (this.nrofCreated > 0) {
+    }
+    
+    /**
+     * Method is not used, but must be implemented due to the class hierarchy
+     * @param m Message to be delivered
+     * @param from sender of the message
+     * @param to receiver of the message
+     */
+    public void messageTransferAborted(Message m, DTNHost from, DTNHost to) {
+        // Method is not used, but must be implemented due to the class hierarchy
+    }
+    
+    /**
+     * @param m Message
+     * @param from sender of the message
+     * @param to receiver of the message
+     * @param finalTarget if the target is thee intended target of the message and if it receives it for the first time
+     */
+    public void messageTransferred(Message m, DTNHost from, DTNHost to, boolean finalTarget) {
+        if (isWarmupID(m.getId())) {
+            return;
+        }
+        
+        if (m.getType() == core.Message.MessageType.ONE_TO_ONE && finalTarget) {
+            this.latencies.add(getSimTime() -
+            this.creationTimes.get(m.getId()) );
+            this.nrofDelivered++;
+            this.hopCounts.add(m.getHops().size() - 1);
+
+            if (m.isResponse()) {
+                this.roundTripTimes.add(getSimTime() -    m.getRequest().getCreationTime());
+            }
+        }
+    }
+    
+    /**
+     * This method is invoked when a new message is created
+     * @param m Message that was recently created
+     */
+    public void newMessage(Message m) {
+        if (isWarmup()) {
+            addWarmupID(m.getId());
+            return;
+        }
+
+        this.creationTimes.put(m.getId(), getSimTime());
+
+        if (m.getType().equals(Message.MessageType.ONE_TO_ONE)) {
+            this.nrofCreated += 1;
+        }
+    }
+    
+    /**
+     * Method is not used, but must be implemented due to the class hierarchy
+     * @param m Message to be delivered
+     * @param from sender of the message
+     * @param to receiver of the message
+     */
+    public void messageTransferStarted(Message m, DTNHost from, DTNHost to) {
+        // Method is not used, but must be implemented due to the class hierarchy
+    }
+    
+    /**
+     * Writes the results of the report to a text file
+     */
+    @Override
+    public void done() {
+        write("Message stats for scenario " + getScenarioName() +
+                "\nsim_time: " + format(getSimTime()));
+        double deliveryProb = 0;
+        
+        if (this.nrofCreated > 0) {
             deliveryProb = (1.0 * this.nrofDelivered) / this.nrofCreated;
-		}
-		
-		String statsText = "created: " + this.nrofCreated +
-				"\ndelivered: " + this.nrofDelivered +
-				"\ndelivery_prob: " + format(deliveryProb)
-				;
-		
-		write(statsText);
-		super.done();
-	}
+        }
+        
+        String statsText = "created: " + this.nrofCreated +
+                "\ndelivered: " + this.nrofDelivered +
+                "\ndelivery_prob: " + format(deliveryProb)
+                ;
+        
+        write(statsText);
+        super.done();
+    }
 }

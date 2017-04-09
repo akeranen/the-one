146ba32b02e168c6050637a596f3f8d5b4a2fed6
Britta Heymann <britta_hey@web.de>
2017-04-09 19:34:17 +0200

    Complete LocalDatabase class.
    refs #230

--- src/core/LocalDatabase.java
+++ src/core/LocalDatabase.java
@@ -1,6 +1,7 @@
 package core;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -12,89 +13,141 @@ import java.util.List;
  * Created by melanie on 07.04.17.
  */
 public class LocalDatabase {
-    /* size of the database in kB */
-    private int size;
-    private List<DisasterData> dataList =new ArrayList<>();
+    private static final int CUBIC = 3;
+
+    /* Parameters for utility function. */
+    private static final double DEFAULT_BASE = 2;
+    private static final double SLOW_AGING_BASE = 1.1;
+    private static final double EQUAL_WEIGHT = 0.5;
+    private static final double SMALLER_WEIGHT = 0.3;
+    private static final double SLOWER_DECREASE_DIVISOR = 2;
+
+    /** Total size of the database in bytes. */
+    private int totalSize;
+    /** Size used by stored data. */
+    private int usedSize;
+
+    /** All stored data */
+    private List<DisasterData> dataList = new ArrayList<>();
+
     /**
-     * when the utility of a data item is below this threshold,
-     * it will be deleted.
+     * Initializes a new instance of the {@link LocalDatabase} class.
+     *
+     * @param totalSize Size of the database in bytes.
      */
-    private double deletionThreshold;
+    public LocalDatabase(int totalSize) {
+        this.totalSize = totalSize;
+    }
 
     /**
      * Adds a dataItem to the database and
-     * checks whether any old data needs to be deleted
-     * @param newDataItem
+     * checks whether any old data needs to be deleted.
+     *
+     * @param newDataItem The data item to add.
+     * @param location The current location, important for deletion check.
+     * @param time The current time, important for deletion check.
      */
     public void add (DisasterData newDataItem, Coord location, double time){
-        dataList.add(newDataItem);
-        size+=newDataItem.getSize();
-        deleteIrrelevantData(location, time);
+        this.dataList.add(newDataItem);
+        this.usedSize += newDataItem.getSize();
+        this.deleteIrrelevantData(location, time);
     }
 
     /**
      * Checks all data items whether their utility
-     * is below the threshold
+     * is below the threshold and removes them if it is.
+     *
+     * @param location The current location.
+     * @param time The current time.
      */
     private void deleteIrrelevantData(Coord location, double time) {
-        computeDeletionThreshold();
-        for (DisasterData dataItem : dataList){
-            if (computeUtility(dataItem, location, time)<deletionThreshold){
-                size-=dataItem.getSize();
-                dataList.remove(dataItem);
+        double deletionThreshold = this.computeDeletionThreshold();
+        for (Iterator<DisasterData> dataIterator = this.dataList.iterator(); dataIterator.hasNext();) {
+            DisasterData dataItem = dataIterator.next();
+            if (LocalDatabase.computeUtility(dataItem, location, time) < deletionThreshold){
+                this.usedSize -= dataItem.getSize();
+                dataIterator.remove();
             }
         }
     }
 
-    private void computeDeletionThreshold() {
-        //TODO
+    /**
+     * Computes a memory-aware deletion threshold s.t. everything below will be deleted.
+     *
+     * @return A threshold between 0 and 1 that is 0 for empty memory and 1 for full memory.
+     */
+    private double computeDeletionThreshold() {
+        double usedMemoryPercentage = (double)this.usedSize / this.totalSize;
+        return Math.pow(usedMemoryPercentage, CUBIC);
     }
 
-    List<DisasterData> getAllDataWithMinimumUtility(double minUtility, Coord location, double time){
-        List <DisasterData> dataWithMinUtility = new ArrayList<>();
-        for (DisasterData dataItem : dataList) {
-            if (computeUtility(dataItem, location, time)>=minUtility){
+    /**
+     * Returns all data that has at least the specified utility if utility is computed w. r. t. the given location and
+     * time.
+     *
+     * @param minUtility The minimum utility the data has to have for it to be returned.
+     * @param location The location to use for utility computation.
+     * @param time The time to use for utility computation.
+     * @return All data with utility at least the given threshold.
+     */
+    public List<DisasterData> getAllDataWithMinimumUtility(double minUtility, Coord location, double time){
+        List<DisasterData> dataWithMinUtility = new ArrayList<>();
+        for (DisasterData dataItem : this.dataList) {
+            if (LocalDatabase.computeUtility(dataItem, location, time) >= minUtility){
                 dataWithMinUtility.add(dataItem);
             }
         }
-
+        return dataWithMinUtility;
     }
 
-    private double computeUtility(DisasterData dataItem, Coord location, double time) {
+    /**
+     * Computes the utility value of a data item given the current location and time.
+     *
+     * @param dataItem The item to compute the utility value for.
+     * @param location The current location.
+     * @param time The current time.
+     * @return The computed utility value, a value between 0 and 1.
+     */
+    private static double computeUtility(DisasterData dataItem, Coord location, double time) {
+        // Factor how much distance influences the utility in comparison to age.
+        double alpha = EQUAL_WEIGHT;
+        // Factor how fast aging occurs, the higher gamma, the faster the aging.
+        double gamma = DEFAULT_BASE;
+
+        /* Adapt alpha and gamma depending on type. */
+        switch (dataItem.getType()){
+            case MAP:
+                // For maps we just regard the distance, as map data does not become outdated
+                // within the disaster time frame.
+                alpha = 1;
+                break;
+            case MARKER:
+                // For markers, both distance and age are important. -> Default values work fine.
+                break;
+            case SKILL:
+                // For skills, the aging is slower, as skills will likely not fade
+                // The importance on distance is also lower
+                alpha = SMALLER_WEIGHT;
+                gamma = SLOW_AGING_BASE;
+                break;
+            case RESOURCE:
+                //For resources the importance of distance is lower
+                alpha = SMALLER_WEIGHT;
+                break;
+            default:
+                throw new UnsupportedOperationException("No implementation for data type " + dataItem.getType() + ".");
+        }
+
+        /* Determine properties to compute utility. */
+
         //Distance between data and current location
         //The farther away an item is, the lower its utility
         double distance = dataItem.getLocation().distance(location);
         //How long it has been since the data item has been created
         //The older an item is, the lower its utility
         double age = time - dataItem.getCreation();
-        //Factor how much distance influences the utility in comparison to result
-        double alpha=0.5;
-        //Factor how fast aging occurs, the higher gamma, the faster the aging
-        double gamma= 2;
 
-        switch (dataItem.getType()){
-            case MAP:
-                //For maps we just regard the distance, as map data does not become outdated
-                //within the disaster time frame
-                alpha=1;
-            case SKILL:
-                //For skills, the aging is slower, as skills will likely not fade
-                //The importance on distance is also lower
-                alpha=0.3;
-                gamma=1.1;
-            case RESOURCE:
-                //For resources the importance of distance is lower
-                alpha=0.3;
-        }
-        //util(dataItem) = α * 2^(-distance/2)  + (1-α) *  γ^(-age)
-        return alpha*Math.pow(2,-(distance/2))+(1-alpha)*Math.pow(gamma,(-age));
-    }
-
-    /**
-     *
-     * @return size of the database in kB
-     */
-    public int getSize() {
-        return size;
+        /* Compute utility. */
+        return alpha * Math.pow(DEFAULT_BASE, -(distance/SLOWER_DECREASE_DIVISOR)) + (1-alpha) * Math.pow(gamma,-age);
     }
 }

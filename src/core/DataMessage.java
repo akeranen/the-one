c005b6b7cfaa47f328ac50e8b6d14f6964252153
Britta Heymann <britta_hey@web.de>
2017-04-12 17:03:55 +0200

    Add DataMessage
    refs #231

---
+++ src/core/DataMessage.java
@@ -0,0 +1,41 @@
+package core;
+
+/**
+ * A message wrapping a data item.
+ *
+ * Created by Britta Heymann on 12.04.2017.
+ */
+public class DataMessage extends Message {
+    private DisasterData data;
+
+    public DataMessage(DTNHost from, DTNHost to, String id, DisasterData data) {
+        // TODO: Change to normal one-to-one priority once we have one.
+        super(from, to, id, data.getSize(), INVALID_PRIORITY);
+        this.data = data;
+    }
+    /**
+     * Returns a replicate of this message (identical except for the unique id)
+     *
+     * @return A replicate of the message
+     */
+    @Override
+    public Message replicate() {
+        Message m = new DataMessage(this.from, this.getTo(), this.id, this.data);
+        super.copyFrom(m);
+        return m;
+    }
+
+    public DisasterData getData() {
+        return data;
+    }
+
+    /**
+     * Gets the message type.
+     *
+     * @return The message type.
+     */
+    @Override
+    public MessageType getType() {
+        return MessageType.DATA;
+    }
+}

5e5b1ea12f5bc89342f1448a6f430a1b09a16048
Britta Heymann <britta_hey@web.de>
2017-04-13 12:33:47 +0200

    Change DatabaseApplication to be attached to 1 host
    Looking at the PingApplication, all applications are attached to a
    single host, though they might not know which host that is.
    
    refs #231

--- src/applications/DatabaseApplication.java
+++ src/applications/DatabaseApplication.java
@@ -1,26 +1,22 @@
 package applications;
 
 import core.Application;
-import core.Connection;
 import core.DTNHost;
 import core.DataMessage;
 import core.DisasterData;
 import core.LocalDatabase;
 import core.Message;
 import core.Settings;
-import core.SimScenario;
-import core.World;
 import input.DisasterDataCreationListener;
+import util.Tuple;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 /**
- * An application handling one local database per host.
+ * An application handling the {@link LocalDatabase} for the {@link DTNHost} the application instance is attached to.
  *
  * Created by Britta Heymann on 12.04.2017.
  */
@@ -39,54 +35,78 @@ public class DatabaseApplication extends Application implements DisasterDataCrea
     public static final String DATABASE_SIZE_RANGE = "db_size";
     /**
      * Database size randomizer seed -setting id ({@value}).
-     * The seed used for the pseudo-random number generator that sets local database sizes.
+     * Part of the seed used for the pseudo-random number generator that sets local database sizes.
+     * The other part of the seed is the {@link DTNHost}'s address.
      */
     public static final String SIZE_RANDOMIZER_SEED = "seed";
 
-    // TODO: Should all these be static? Does every host have its own application?
+    /** Application ID */
+    public static final String APP_ID = "AdHocNetworksInDisasterScenarios";
+
+    /* Some properties that are the same across all application instances. */
     private double utilityThreshold;
-    private Map<DTNHost, LocalDatabase> hostsToDatabases;
-    private Random random;
+    private int[] databaseSizeRange;
+    private int seed;
 
-    public DatabaseApplication() {
+    /** The host this application instance is attached to. */
+    private DTNHost host;
+
+    /** The host's database. */
+    private LocalDatabase database;
+
+    /** Stash for created data if attached host is not known yet. */
+    private List<Tuple<DTNHost, DisasterData>> stashedCreationData = Collections.synchronizedList(new ArrayList<>());
+
+    /**
+     * Creates a new instance of the {@link DatabaseApplication} class.
+     * @param s A settings object to read properties from.
+     */
+    public DatabaseApplication(Settings s) {
         super();
-        this.hostsToDatabases = Collections.synchronizedMap(new HashMap<>());
-    }
-
-    private DatabaseApplication(DatabaseApplication application) {
-        super(application);
-        this.hostsToDatabases = Collections.synchronizedMap(application.hostsToDatabases);
-        this.utilityThreshold = application.utilityThreshold;
-    }
-
-    private void initialize() {
-        Settings s = new Settings(this.getClass().getSimpleName());
 
         /* Read properties from settings */
         this.utilityThreshold = s.getDouble(UTILITY_THRESHOLD);
-        int[] databaseSizeRange = s.getCsvInts(DATABASE_SIZE_RANGE, Settings.EXPECTED_VALUE_NUMBER_FOR_RANGE);
-        int seed = s.getInt(SIZE_RANDOMIZER_SEED);
+        this.databaseSizeRange = s.getCsvInts(DATABASE_SIZE_RANGE, Settings.EXPECTED_VALUE_NUMBER_FOR_RANGE);
+        this.seed = s.getInt(SIZE_RANDOMIZER_SEED);
 
         /* Check they are valid. */
         DatabaseApplication.checkUtilityThreshold(this.utilityThreshold);
-        DatabaseApplication.checkDatabaseSizeRange(databaseSizeRange);
+        DatabaseApplication.checkDatabaseSizeRange(this.databaseSizeRange);
 
-        /* Create randomizer. */
-        this.random = new Random(seed);
-
-        /* Initialize host -> database map. */
-        World world = SimScenario.getInstance().getWorld();
-        for (DTNHost host : world.getHosts()) {
-            hostsToDatabases.put(host, new LocalDatabase(host, selectRandomInteger(databaseSizeRange)));
-        }
+        /* Set App ID. */
+        super.setAppID(APP_ID);
     }
 
+    /**
+     * Copy constructor for the {@link DatabaseApplication} class.
+     *
+     * @param application The {@link DatabaseApplication} instance to copy.
+     */
+    private DatabaseApplication(DatabaseApplication application) {
+        super(application);
+        this.utilityThreshold = application.utilityThreshold;
+        this.databaseSizeRange = application.databaseSizeRange;
+        this.seed = application.seed;
+    }
+
+    /**
+     * Checks that the utility threshold is between 0 and 1.
+     *
+     * @param threshold Threshold to check.
+     * @throws IllegalArgumentException for wrong thresholds.
+     */
     private static void checkUtilityThreshold(double threshold) {
         if (threshold < 0 || threshold > 1) {
             throw new IllegalArgumentException("Utility threshold must be between 0 and 1.");
         }
     }
 
+    /**
+     * Checks that the sizes form a non-empty range and are all non-negative.
+     *
+     * @param databaseSizeRange Database size range to check.
+     * @throws IllegalArgumentException for a wrong range.
+     */
     private static void checkDatabaseSizeRange(int[] databaseSizeRange) {
         if (databaseSizeRange[1] < databaseSizeRange[0]) {
             throw new IllegalArgumentException("Database size range must be non-empty!");
@@ -96,30 +116,28 @@ public class DatabaseApplication extends Application implements DisasterDataCrea
         }
     }
 
-    private int selectRandomInteger(int[] range) {
-        return range[0] + this.random.nextInt(range[1] + 1);
-    }
-
     /**
-     * Called when a {@link DisasterData} got created by a {@link input.DisasterDataCreateEvent}.
-     * Adds the newly created data to the host's local database.
+     * Called every simulation cycle.
      *
-     * @param creator   {@link DTNHost} that created the data.
-     * @param data The created {@link DisasterData}.
+     * If the application instance is not aware of the host it is attached to at this point, it will use that new
+     * knowledge.
+     *
+     * @param host The host this application instance is attached to.
      */
     @Override
-    public void disasterDataCreated(DTNHost creator, DisasterData data) {
-        LocalDatabase creatorDatabase = hostsToDatabases.get(creator);
-        creatorDatabase.add(data);
+    public void update(DTNHost host) {
+        if (this.isInitialized()) {
+            this.initialize(host);
+        }
     }
 
     /**
      * This method handles application functionality related to
      * processing of the bundle. Application handles a messages,
-     * which arrives to the node hosting this application. After
-     * performing application specific handling, this method returns
-     * a list of messages. If node wishes to continue forwarding the
-     * incoming
+     * which arrives to the node hosting this application.
+     *
+     * In this case, all data messages to the host this application is attached to are unwrapped, the data is stored,
+     * and those messages are not used any further.
      *
      * @param msg  The incoming message.
      * @param host The host this application instance is attached to.
@@ -129,57 +147,109 @@ public class DatabaseApplication extends Application implements DisasterDataCrea
      */
     @Override
     public Message handle(Message msg, DTNHost host) {
-        if (msg instanceof DataMessage) {
-            LocalDatabase database = hostsToDatabases.get(host);
-            database.add(((DataMessage) msg).getData());
+        // If we don't know who the application is attached to yet, use the new knowledge for initialization.
+        if (!this.isInitialized()) {
+            this.initialize(host);
+        }
 
-            // Never put data message into buffer.
+        // If the message is a data message sent to the host the application instance is attached to, unwrap and store
+        // the data and don't forward the message any further.
+        if (msg instanceof DataMessage && msg.getTo().equals(this.host)) {
+            this.database.add(((DataMessage) msg).getData());
             return null;
         } else {
+            // Else act as you would usually do.
             return msg;
         }
     }
 
     /**
-     * Creates database synchronization messages that should be sent for the provided {@link Connection}.
+     * Creates database synchronization messages that should be sent to the provided receiver.
      *
-     * @param connection Connection to find data messages for.
+     * @param databaseOwner The DTNHost this instance of the application is attached to.
+     * @param receiver DTNHost to find data messages for.
      * @return The created messages.
      */
-    public List<DataMessage> createDataMessagesFor(Connection connection) {
-        DTNHost sender = connection.getInitiator();
-        DTNHost receiver = connection.getOtherNode(sender);
-        LocalDatabase senderDatabase = this.hostsToDatabases.get(sender);
+    public List<DataMessage> createDataMessages(DTNHost databaseOwner, DTNHost receiver) {
+        // If we don't know who the application is attached to yet, use the new knowledge for initialization.
+        if (!this.isInitialized()) {
+            this.initialize(databaseOwner);
+        }
 
-        List<DisasterData> interestingData = senderDatabase.getAllDataWithMinimumUtility(this.utilityThreshold);
+        List<DisasterData> interestingData = this.database.getAllDataWithMinimumUtility(this.utilityThreshold);
         List<DataMessage> messages = new ArrayList<>(interestingData.size());
         for (DisasterData data : interestingData) {
             // Create message out of the data.
             // DataMessages, like 1-to-1s, always have lowest priority (0).
-            messages.add(new DataMessage(sender, receiver, "DbSync", data, 0));
+            messages.add(new DataMessage(this.host, receiver, "DbSync", data, 0));
         }
         return messages;
     }
 
     /**
-     * Called every simulation cycle.
+     * Called when a {@link DisasterData} got created by a {@link input.DisasterDataCreateEvent}.
      *
-     * @param host The host this application instance is attached to.
+     * Adds the newly created data to local database if the creator is the host this application instance is attached
+     * to.
+     *
+     * @param creator   {@link DTNHost} that created the data.
+     * @param data The created {@link DisasterData}.
      */
     @Override
-    public void update(DTNHost host) {
-        if (this.isInitialized()) {
-            this.initialize();
+    public void disasterDataCreated(DTNHost creator, DisasterData data) {
+        // If the host is not known yet, stash the information until it is known.
+        if (!this.isInitialized()) {
+            this.stashedCreationData.add(new Tuple<>(creator, data));
         }
+
+        // Otherwise, check if the creator is the attached host and add the data to database if that's the case.
+        if (creator.equals(this.host)) {
+            this.database.add(data);
+        }
+    }
+
+    /**
+     * Checks whether we already know the host this application instance is attached to.
+     *
+     * @return A value indicating whether we know the host this application instance is attached to.
+     */
+    private boolean isInitialized() {
+        return this.host == null;
+    }
+
+    /**
+     * Initializes instance fields that are dependent on the host the application instance is attached to.
+     *
+     * @param host The host the application instance is attached to.
+     */
+    private void initialize(DTNHost host) {
+        this.host = host;
+        this.database = new LocalDatabase(this.host, selectRandomDatabaseSize());
+
+        // We can now handle our stash.
+        for (Tuple<DTNHost, DisasterData> creatorDataTuple : this.stashedCreationData) {
+            if (creatorDataTuple.getKey().equals(this.host)) {
+                this.database.add(creatorDataTuple.getValue());
+            }
+        }
+        this.stashedCreationData.clear();
+    }
+
+    /**
+     * Selects a valid random database size.
+     * @return The selected size.
+     */
+    private int selectRandomDatabaseSize() {
+        // Random generators need to be different for each host to have different database sizes.
+        // Create one.
+        Random pseudoRandom = new Random(this.seed * this.host.getAddress());
+        // Then, randomly choose a database size.
+        return this.databaseSizeRange[0] + pseudoRandom.nextInt(this.databaseSizeRange[1] + 1);
     }
 
     @Override
     public Application replicate() {
-        // TODO: When is this used? Is the following the correct solution for it?
         return new DatabaseApplication(this);
     }
 
-    private boolean isInitialized() {
-        return this.hostsToDatabases == null;
-    }
 }

2ae37a76fbf2ef40b49a7f705e6897711d7b26d1
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-04 13:56:03 +0200

    Adapt scripts for OCuLUS

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
-targetdir=target
+targetdir=target/production/Simulator
 
 if [ ! -d "$targetdir" ]; then mkdir $targetdir; fi
 
 javac -sourcepath src -d $targetdir -extdirs lib/ src/core/*.java src/movement/*.java src/report/*.java src/routing/*.java src/gui/*.java src/input/*.java src/applications/*.java src/interfaces/*.java
 
-if [ ! -d "$targetdir/gui/buttonGraphics" ]; then cp -R src/gui/buttonGraphics target/gui/; fi
+if [ ! -d "$targetdir/gui/buttonGraphics" ]; then cp -R src/gui/buttonGraphics $targetdir/gui/; fi
 	
--- one.sh
+++ one.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xmx512M -cp target:lib/javax.json-1.0.4.jar:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+java -Xmx8192M -cp target/production/Simulator:lib/javax.json-1.0.4.jar:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*

42af7ccf2e4ff540bbaf072ecc1a4963c04d5495
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-05 16:13:35 +0200

    Adapt startscript to allow the use of up to 1TB of RAM

--- one.sh
+++ one.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xmx8192M -cp target/production/Simulator:lib/javax.json-1.0.4.jar:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+java -Xmx1048576M -cp target/production/Simulator:lib/javax.json-1.0.4.jar:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*

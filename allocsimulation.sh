e48631ba0795095acae142fdc4e5ad6fddfb8b48
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-11 11:52:12 +0200

    clean up before and after everything else

--- allocsimulation.sh
+++ allocsimulation.sh
@@ -2,11 +2,12 @@
 #CCS --island OCULUS
 #CCS -N pgandiSim
 #CCS --res=rset=1:ncpus=2:mem=256g
-#CCS -t 7d
+#CCS -t 1h
 #CCS -m abe
 #CCS --output=out.txt
 #CCS --stderr=err.txt
-nice -n 15 ./compile.sh
 rm reports/* out.txt err.txt
+nice -n 15 ./compile.sh
 ./one.sh -b 1 configurations/evaluation/settingsScenarioShort.txt
 nice -n 15 tar -czf "result$(date '+%d%m%Y_%H%M%S').tar.gz" out.txt err.txt reports
+rm reports/* out.txt err.txt

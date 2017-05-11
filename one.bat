47d75a9d92d8c4faa4a3fb2dc38d70ca328ac6f0
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-11 15:27:48 +0200

    add initial heap size to the batch file

--- one.bat
+++ one.bat
@@ -1,1 +1,1 @@
-java -mx256G -cp target/production/Simulator;lib/javax.json-1.0.4.jar;lib/ECLA.jar;lib/DTNConsoleConnection.jar core.DTNSim %*
+java -ms1G -mx256G -cp target/production/Simulator;lib/javax.json-1.0.4.jar;lib/ECLA.jar;lib/DTNConsoleConnection.jar core.DTNSim %*

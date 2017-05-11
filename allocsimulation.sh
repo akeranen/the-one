cd8151639bbb8f12ffa6817b119e334527df0b5e
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-11 10:30:19 +0200

    fix the OCuLUS allocation script

--- allocsimulation.sh
+++ allocsimulation.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/sh
 #CCS --island OCULUS
-#CCS -g HPC-SNF
 #CCS -N pgandiSim
-#CCS --res=rset=1:ncpus=8:mem=256g
-#CCS -t 1h
-#CCS -M amaehrle@mail.uni-paderborn.de
+#CCS --res=rset=1:ncpus=2:mem=256g
+#CCS -t 168h
 #CCS -m abe
 #CCS --output=out.txt
 #CCS --stderr=err.txt
-./one.sh -b 1 configurations/evaluation/SettingsScenarioShort.txt
+./one.sh -b 1 configurations/evaluation/settingsScenarioShort.txt

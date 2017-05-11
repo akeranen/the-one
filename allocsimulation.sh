920d42eeafa4a5d31a7325b18453407cfc493827
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-11 11:16:37 +0200

    add pre- and post-processing to the job script

--- allocsimulation.sh
+++ allocsimulation.sh
@@ -6,4 +6,7 @@
 #CCS -m abe
 #CCS --output=out.txt
 #CCS --stderr=err.txt
+nice -n 15 ./compile.sh
+rm reports/* out.txt err.txt
 ./one.sh -b 1 configurations/evaluation/settingsScenarioShort.txt
+nice -n 15 tar -czf "result$(date '+%d%m%Y_%H%M%S').tar.gz" out.txt err.txt reports

871b4a60c90bec952b7292d3fa9c5041a22793bf
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-11 10:30:51 +0200

    set the correct heap size

--- one.sh
+++ one.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xmx1048576M -cp target/production/Simulator:lib/javax.json-1.0.4.jar:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+java -mx256G -cp target/production/Simulator:lib/javax.json-1.0.4.jar:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*

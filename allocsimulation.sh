6201aa98720b5053d9953120c1442873061b469f
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-11 15:26:32 +0200

    fix logging and compiling by adding another script

--- allocsimulation.sh
+++ allocsimulation.sh
@@ -1,13 +1,11 @@
-#!/usr/bin/sh
+#!/bin/sh
 #CCS --island OCULUS
 #CCS -N pgandiSim
 #CCS --res=rset=1:ncpus=2:mem=256g
-#CCS -t 1h
+#CCS -t 7d
 #CCS -m abe
 #CCS --output=out.txt
 #CCS --stderr=err.txt
-rm reports/* out.txt err.txt
-nice -n 15 ./compile.sh
 ./one.sh -b 1 configurations/evaluation/settingsScenarioShort.txt
 nice -n 15 tar -czf "result$(date '+%d%m%Y_%H%M%S').tar.gz" out.txt err.txt reports
 rm reports/* out.txt err.txt
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+nice -n 15 ./compile.sh
+ccsalloc allocsimulation.sh

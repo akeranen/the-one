cc175ce194b8871c5602ba8008a1432d56e1b0e9
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-11 11:46:22 +0200

    prevent mkdir from failing if the whole path is not existing

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 targetdir=target/production/Simulator
 
-if [ ! -d "$targetdir" ]; then mkdir $targetdir; fi
+if [ ! -d "$targetdir" ]; then mkdir -p $targetdir; fi
 
 javac -sourcepath src -d $targetdir -extdirs lib/ src/core/*.java src/movement/*.java src/report/*.java src/routing/*.java src/gui/*.java src/input/*.java src/applications/*.java src/interfaces/*.java
 

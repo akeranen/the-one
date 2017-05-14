ac0720ba2251a1c542c49b3d7fd472eff3d7684a
Britta Heymann <britta_hey@web.de>
2017-05-14 22:11:19 +0200

    Implement EncounterValueManager.java
    refs #300

---
+++ src/routing/util/EncounterValueManager.java
@@ -0,0 +1,110 @@
+package routing.util;
+
+import core.Settings;
+import core.SettingsError;
+import core.SimClock;
+
+/**
+ * Manages a node's encounter value, a rating mechanism to measure a host's popularity.
+ *
+ * The measure is implemented as described in S. C. Nelson, M. Bakht and R. Kravets: Encounter-Based Routing in
+ * Disruption-Tolerant Networks, IEEE INFOCOM, 846-854.
+ *
+ * Created by Britta Heymann on 14.05.2017.
+ */
+public class EncounterValueManager {
+    /**
+     * Length of a time window in seconds -setting id ({@value}).
+     * How many seconds a time window lasts. After each time window, the encounter value gets updated using the most
+     * recent data.
+     */
+    public static final String WINDOW_LENGTH_S = "windowLength";
+
+    /**
+     * Importance of recent encounters relative to previous encounters -setting id ({@value}).
+     * A value between 0 and 1.
+     * The weight the newly computed encounters per time window rate gets when the old rate is updated.
+     */
+    public static final String AGING_FACTOR = "agingFactor";
+
+    /**
+     * The value of the encounter value ratio if the two compared hosts are equally social.
+     */
+    private static final double EQUALLY_SOCIAL = 0.5;
+    /**
+     * The maximal possible value when checking if the encounter value is 0.
+     */
+    private static final double NO_ENCOUNTERS = 0.000001;
+
+    private double windowLength;
+    private double agingFactor;
+
+    private double encounterValue;
+    private int currentWindowCounter;
+    private double nextWindowEnd;
+
+    /**
+     * Initializes a new instance of the {@link EncounterValueManager} class.
+     * @param settings Settings to use.
+     */
+    public EncounterValueManager(Settings settings) {
+        this.windowLength = settings.getDouble(WINDOW_LENGTH_S);
+        this.agingFactor = settings.getDouble(AGING_FACTOR);
+        if (this.agingFactor < 0 || this.agingFactor > 1) {
+            throw new SettingsError("Aging factor has to be between 0 and 1!");
+        }
+        this.nextWindowEnd = windowLength;
+    }
+
+    /**
+     * Copy constructor. All constants are copied over, but the encounter ratios are not.
+     */
+    public EncounterValueManager(EncounterValueManager manager) {
+        this.windowLength = manager.windowLength;
+        this.agingFactor = manager.agingFactor;
+        this.nextWindowEnd = manager.nextWindowEnd;
+    }
+
+    /**
+     * Adds a new encounter that will be considered when computing the encounter value.
+     */
+    public void addEncounter() {
+        this.currentWindowCounter++;
+    }
+
+    /**
+     * Gets the encounter value.
+     * @return The encounter value.
+     */
+    public double getEncounterValue() {
+        return this.encounterValue;
+    }
+
+    /**
+     * Updates the encounter value if a time window has ended.
+     * Call this method in each simulation step.
+     */
+    public void update() {
+        while (SimClock.getTime() >= this.nextWindowEnd) {
+            this.encounterValue =
+                    this.agingFactor * this.currentWindowCounter + (1 - this.agingFactor) * this.encounterValue;
+            this.currentWindowCounter = 0;
+            this.nextWindowEnd += this.windowLength;
+        }
+    }
+
+    /**
+     * Computes a ratio between the encounter value managed by this instance and the provided encounter value.
+     * A ratio less than 0.5 signifies that the other host is more social than the one this manager is attached to, a
+     * ratio higher than 0.5 signifies the opposite.
+     *
+     * @param otherEncounterValue The encounter value to compare the managed encounter value to.
+     * @return A ratio between 0 and 1.
+     */
+    public double computeEncounterValueRatio(double otherEncounterValue) {
+        if (this.encounterValue < NO_ENCOUNTERS && otherEncounterValue < NO_ENCOUNTERS) {
+            return EQUALLY_SOCIAL;
+        }
+        return otherEncounterValue / (this.encounterValue + otherEncounterValue);
+    }
+}

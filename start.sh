aa3c92c22148b9dde77aeb0339dbef1392c79864
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-05-18 11:11:07 +0200

    add cleanup operations in the start script

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+chmod +x compile.sh allocsimulation.sh one.sh
+nice -n 15 tar -czf "beforestart$(date '+%d%m%Y_%H%M%S').tar.gz" out.txt err.txt reports
+rm reports/* out.txt err.txt
 nice -n 15 ./compile.sh
 ccsalloc allocsimulation.sh

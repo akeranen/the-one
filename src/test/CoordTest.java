6c52a4d177d09a898a69a04d0676f99d6a185691
Melanie Bruns <mbruns42@mail.upb.de>
2017-05-25 11:27:42 +0200

    Replace assertTrue by assert[Not]Equals and remove symmetry check

--- src/test/CoordTest.java
+++ src/test/CoordTest.java
@@ -4,36 +4,33 @@
  */
 package test;
 
-import junit.framework.TestCase;
 import core.Coord;
 
-public class CoordTest extends TestCase {
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-	public void testHashCode() {
-		Coord c1 = new Coord(1,1);
-		Coord c2 = new Coord(1,1);
-		Coord c3 = new Coord(1,1);
-		Coord c4 = new Coord(2,3);
-		Coord c5 = new Coord(3,2);
-		Coord c6 = new Coord(-2,-3);
-		Coord c7 = new Coord(-2,-3);
+public class CoordTest{
 
-		//hash must be reflexive and consistent
-		assertTrue(c1.hashCode() == c1.hashCode());
-		//hash must be symmetric
-		assertTrue(c1.hashCode() == c2.hashCode());
-		assertTrue(c2.hashCode() == c1.hashCode());
-		//hash must be transitive
-		assertTrue(c1.hashCode() == c2.hashCode() && c1.hashCode() == c3.hashCode() && c2.hashCode() == c3.hashCode());
-		assertTrue(c1.hashCode() != c4.hashCode());
-		assertTrue(c4.hashCode() != c1.hashCode());
-		assertTrue(c4.hashCode() != c5.hashCode());
-		assertTrue(c6.hashCode() == c7.hashCode());
-		assertTrue(c4.hashCode() != c6.hashCode());
-		//Cached hash must be recomputed after translation
-		c6.translate(1, 1);
-		assertTrue(c6.hashCode() != c7.hashCode());
+    @Test
+    public void testHashCode() {
+        final Coord c1 = new Coord(1,1);
+        final Coord c2 = new Coord(1,1);
+        final Coord c3 = new Coord(2,3);
+        final Coord c4 = new Coord(3,2);
+        final Coord c5 = new Coord(-2,-3);
+        final Coord c6 = new Coord(-2,-3);
 
-	}
+        assertEquals("Hash function has to be consistent", c1.hashCode(), c1.hashCode());
+        assertEquals("Hash for two coordinate with the same values should be equal", c1.hashCode(), c2.hashCode());
+        assertNotEquals("Hash for different coordinate should be different.",c1.hashCode(), c3.hashCode());
+        assertNotEquals("Hash should depend on coordinate ordering.", c3.hashCode(), c4.hashCode());
+        assertEquals("Hash for two coordinate with the same values should be equal",c5.hashCode(), c6.hashCode());
+        assertNotEquals("Hash value should be different for positive and negative values", c3.hashCode(),c5.hashCode());
+
+        //Move point to see if the hash changes
+        c5.translate(1, 1);
+        assertNotEquals("Cached hash must be recomputed after translation", c5.hashCode(), c6.hashCode());
+
+    }
 
 }

0b52f56b5a82e2e47c91a743b2cc731e59339eb6
Melanie Bruns <mbruns42@mail.upb.de>
2017-05-27 18:04:12 +0200

    Add quick return in case of just one interface (relevant for our test)

--- src/core/DTNHost.java
+++ src/core/DTNHost.java
@@ -185,13 +185,16 @@ public class DTNHost implements Comparable<DTNHost>, Addressable {
     }
 
     /**
-     * Returns a copy of the list of connections this host has with other hosts
+     * Returns a list of connections this host has with other hosts
      *
-     * @return a copy of the list of connections this host has with other hosts
+     * @return a list of connections this host has with other hosts
      */
     public List<Connection> getConnections() {
-        List<Connection> lc = new ArrayList<Connection>();
+        if (net.size() == 1){
+            return net.get(0).getConnections();
+        }
 
+        List<Connection> lc = new ArrayList<>();
         for (NetworkInterface i : net) {
             lc.addAll(i.getConnections());
         }

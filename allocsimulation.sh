7576a56579a933ac5a273d29f16d653c091c3b5c
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-06-08 15:37:19 +0200

    make sure both runs for each configuration are executed

--- allocsimulation.sh
+++ allocsimulation.sh
@@ -6,6 +6,6 @@
 #CCS -m abe
 #CCS --output=out.txt
 #CCS --stderr=err.txt
-./one.sh -b 1 configurations/evaluation/settingsScenarioShort.txt
+./one.sh -b 2 configurations/evaluation/settingsScenarioShort.txt
 nice -n 15 tar -czf "result$(date '+%d%m%Y_%H%M%S').tar.gz" out.txt err.txt reports
 rm reports/* out.txt err.txt

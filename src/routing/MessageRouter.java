d73093057307066583cb71a03103c866c8beeb8f
Melanie Bruns <mbruns42@mail.upb.de>
2017-06-09 14:47:23 +0200

    Update occupancy when adding/removing messages

--- src/routing/MessageRouter.java
+++ src/routing/MessageRouter.java
@@ -105,6 +105,8 @@ public abstract class MessageRouter {
 	private DTNHost host;
 	/** size of the buffer */
 	private long bufferSize;
+    /** Amount of buffer that is occupied by messages */
+    private long occupancy;
 	/** TTL for all messages */
 	protected int msgTtl;
 	/** Queue mode for sending messages */
@@ -293,14 +295,9 @@ public abstract class MessageRouter {
 	 * size isn't defined)
 	 */
 	public long getFreeBufferSize() {
-		long occupancy = 0;
 
 		if (this.getBufferSize() == Integer.MAX_VALUE) {
 			return Integer.MAX_VALUE;
-		}
-
-		for (Message m : getMessageCollection()) {
-			occupancy += m.getSize();
 		}
 
 		return this.getBufferSize() - occupancy;
@@ -457,6 +454,9 @@ public abstract class MessageRouter {
 	 * message, if false, nothing is informed.
 	 */
 	protected void addToMessages(Message m, boolean newMessage) {
+	    if (!this.messages.containsKey(m.getId())){
+            occupancy += m.getSize();
+        }
 		this.messages.put(m.getId(), m);
 
 		if (newMessage) {
@@ -479,6 +479,7 @@ public abstract class MessageRouter {
 	 */
 	protected Message removeFromMessages(String id) {
 		Message m = this.messages.remove(id);
+		occupancy-=m.getSize();
 		return m;
 	}
 

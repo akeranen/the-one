2bfd43ac01e9c293641b09e46a22c1dac69ecc95
amaehrle <amaehrle@mail.uni-paderborn.de>
2017-06-16 14:22:45 +0200

    lower the default memory allocation to 128GB

--- one.sh
+++ one.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms1G -Xmx1000G -cp target:lib/javax.json-1.0.4.jar:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+java -Xms1G -Xmx128G -cp target:lib/javax.json-1.0.4.jar:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*

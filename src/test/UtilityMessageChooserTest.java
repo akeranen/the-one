9d15cc00893a7cb62a713afecd1e5a26b9937fd7
Britta Heymann <britta_hey@web.de>
2017-06-29 18:54:35 +0200

    Add more to UtilityMessageChooserTest
    refs #354

--- src/test/UtilityMessageChooserTest.java
+++ src/test/UtilityMessageChooserTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import applications.DatabaseApplication;
+import core.BroadcastMessage;
 import core.CBRConnection;
 import core.Connection;
 import core.DTNHost;
@@ -9,6 +10,7 @@ import core.DisasterData;
 import core.Message;
 import core.SettingsError;
 import core.SimClock;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,7 +22,9 @@ import routing.util.DatabaseApplicationUtil;
 import util.Tuple;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -39,9 +43,12 @@ public class UtilityMessageChooserTest {
 
     private TestSettings settings;
     private UtilityMessageChooser chooser;
+    private TestUtils utils;
     private DTNHost attachedHost;
     private DTNHost neighbor1;
     private DTNHost neighbor2;
+
+    private SimClock clock = SimClock.getInstance();
 
     public UtilityMessageChooserTest() {
         // Empty constructor for "Classes and enums with private members should hava a constructor" (S1258).
@@ -53,18 +60,23 @@ public class UtilityMessageChooserTest {
         this.settings = new TestSettings();
         DisasterRouterTestUtils.addDisasterRouterSettings(this.settings);
 
-        TestUtils utils = new TestUtils(new ArrayList<>(), new ArrayList<>(), this.settings);
+        this.utils = new TestUtils(new ArrayList<>(), new ArrayList<>(), this.settings);
         MessageRouter routerProto = new DisasterRouter(this.settings);
         routerProto.addApplication(new DatabaseApplication(this.settings));
-        utils.setMessageRouterProto(routerProto);
-        this.attachedHost = utils.createHost();
-        this.neighbor1 = utils.createHost();
-        this.neighbor2 = utils.createHost();
+        this.utils.setMessageRouterProto(routerProto);
+
+        this.attachedHost = this.utils.createHost();
+        this.neighbor1 = this.utils.createHost();
+        this.neighbor2 = this.utils.createHost();
 
         this.chooser = new UtilityMessageChooser(this.attachedHost.getRouter());
         this.chooser.setAttachedHost(this.attachedHost);
+    }
 
-        this.settings.setNameSpace(UtilityMessageChooser.UTILITY_MESSAGE_CHOOSER_NS);
+    @After
+    public void cleanUp() {
+        SimClock.reset();
+        DTNHost.reset();
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -80,6 +92,7 @@ public class UtilityMessageChooserTest {
     @Test(expected = SettingsError.class)
     public void testConstructorThrowsOnNegativeProphetPlusWeight() {
         // Set negative weight.
+        this.settings.setNameSpace(UtilityMessageChooser.UTILITY_MESSAGE_CHOOSER_NS);
         this.settings.putSetting(UtilityMessageChooser.PROPHET_PLUS_WEIGHT, Double.toString(NEGATIVE_VALUE));
         // Make sure weights still add up to 1.
         increaseWeight(this.settings, UtilityMessageChooser.REPLICATIONS_DENSITY_WEIGHT, OPPOSITE_OF_NEGATIVE_VALUE);
@@ -90,6 +103,7 @@ public class UtilityMessageChooserTest {
     @Test(expected = SettingsError.class)
     public void testConstructorThrowsOnNegativeDeliveryPredictabilityWeight() {
         // Set negative weight.
+        this.settings.setNameSpace(UtilityMessageChooser.UTILITY_MESSAGE_CHOOSER_NS);
         this.settings.putSetting(
                 UtilityMessageChooser.DELIVERY_PREDICTABILITY_WEIGHT, Double.toString(NEGATIVE_VALUE));
         // Make sure weights still add up to 1.
@@ -101,6 +115,7 @@ public class UtilityMessageChooserTest {
     @Test(expected = SettingsError.class)
     public void testConstructorThrowsOnNegativePowerWeight() {
         // Set negative weight.
+        this.settings.setNameSpace(UtilityMessageChooser.UTILITY_MESSAGE_CHOOSER_NS);
         this.settings.putSetting(UtilityMessageChooser.POWER_WEIGHT, Double.toString(NEGATIVE_VALUE));
         // Make sure weights still add up to 1.
         increaseWeight(this.settings, UtilityMessageChooser.DELIVERY_PREDICTABILITY_WEIGHT, OPPOSITE_OF_NEGATIVE_VALUE);
@@ -111,6 +126,7 @@ public class UtilityMessageChooserTest {
     @Test(expected = SettingsError.class)
     public void testConstructorThrowsOnNegativeReplicationsDensityWeight() {
         // Set negative weight.
+        this.settings.setNameSpace(UtilityMessageChooser.UTILITY_MESSAGE_CHOOSER_NS);
         this.settings.putSetting(UtilityMessageChooser.REPLICATIONS_DENSITY_WEIGHT, Double.toString(NEGATIVE_VALUE));
         // Make sure weights still add up to 1.
         increaseWeight(this.settings, UtilityMessageChooser.PROPHET_PLUS_WEIGHT, OPPOSITE_OF_NEGATIVE_VALUE);
@@ -121,6 +137,7 @@ public class UtilityMessageChooserTest {
     @Test(expected = SettingsError.class)
     public void testConstructorThrowsOnNegativeEncounterValueWeight() {
         // Set negative weight.
+        this.settings.setNameSpace(UtilityMessageChooser.UTILITY_MESSAGE_CHOOSER_NS);
         this.settings.putSetting(UtilityMessageChooser.ENCOUNTER_VALUE_WEIGHT, Double.toString(NEGATIVE_VALUE));
         // Make sure weights still add up to 1.
         increaseWeight(this.settings, UtilityMessageChooser.PROPHET_PLUS_WEIGHT, OPPOSITE_OF_NEGATIVE_VALUE);
@@ -130,18 +147,21 @@ public class UtilityMessageChooserTest {
 
     @Test(expected = SettingsError.class)
     public void testConstructorThrowsForWeightNotAddingTo1() {
+        this.settings.setNameSpace(UtilityMessageChooser.UTILITY_MESSAGE_CHOOSER_NS);
         increaseWeight(this.settings, UtilityMessageChooser.POWER_WEIGHT, OPPOSITE_OF_NEGATIVE_VALUE);
         new UtilityMessageChooser(new DisasterRouter(this.settings));
     }
 
     @Test(expected = SettingsError.class)
     public void testConstructorThrowsForNegativeThreshold() {
+        this.settings.setNameSpace(UtilityMessageChooser.UTILITY_MESSAGE_CHOOSER_NS);
         this.settings.putSetting(UtilityMessageChooser.UTILITY_THRESHOLD, Double.toString(NEGATIVE_VALUE));
         new UtilityMessageChooser(new DisasterRouter(this.settings));
     }
 
     @Test(expected = SettingsError.class)
     public void testConstructorThrowsForThresholdAbove1() {
+        this.settings.setNameSpace(UtilityMessageChooser.UTILITY_MESSAGE_CHOOSER_NS);
         this.settings.putSetting(UtilityMessageChooser.UTILITY_THRESHOLD, Double.toString(VALUE_ABOVE_ONE));
         new UtilityMessageChooser(new DisasterRouter(this.settings));
     }
@@ -237,12 +257,6 @@ public class UtilityMessageChooserTest {
                 DOUBLE_COMPARISON_DELTA);
     }
 
-    // check router is set correctly
-    // Check setAttachedhost works correclty
-    // check router is set correctly (replicate)
-    // check messages with low utility are not sent. Influenced by: other's delivery pred, other's power,
-    // message's replications density, encountervalue ratio, utility threshold
-
     /**
      * Checks {@link UtilityMessageChooser#findOtherMessages(Collection, List)} returns data messages for all
      * connections.
@@ -279,7 +293,24 @@ public class UtilityMessageChooserTest {
      */
     @Test
     public void testFindOtherMessagesDoesNotReturnDirectMessages() {
+        // Create message to neighbor 1.
+        Message m = new Message(this.attachedHost, neighbor1, "M1", 0);
+        this.attachedHost.createNewMessage(m);
 
+        // Call findOtherMessages with two connections, one of them to neighbor 1.
+        List<Connection> connections = new ArrayList<>();
+        connections.add(UtilityMessageChooserTest.createConnection(this.attachedHost, neighbor1));
+        connections.add(UtilityMessageChooserTest.createConnection(this.attachedHost, neighbor2));
+        Collection<Tuple<Message, Connection>> messages =
+                this.chooser.findOtherMessages(Collections.singletonList(m), connections);
+
+        // Make sure the direct message was not returned.
+        Assert.assertEquals("Expected different number of chosen messages.", 1, messages.size());
+        Assert.assertFalse(
+                "Direct message should not have been returned.",
+                this.messageToHostsExists(messages, m.getId(), neighbor1));
+        Assert.assertTrue(
+                "Message to second neighbor expected.", this.messageToHostsExists(messages, m.getId(), neighbor2));
     }
 
     /**
@@ -288,7 +319,25 @@ public class UtilityMessageChooserTest {
      */
     @Test
     public void testFindOtherMessagesDoesNotReturnKnownMessages() {
+        // Create message which is known by neighbor 1.
+        Message m = new Message(this.attachedHost, this.utils.createHost(), "M1", 0);
+        this.attachedHost.createNewMessage(m);
+        this.neighbor1.createNewMessage(m);
 
+        // Call findOtherMessages with two connections, one of them to neighbor 1.
+        List<Connection> connections = new ArrayList<>();
+        connections.add(UtilityMessageChooserTest.createConnection(this.attachedHost, neighbor1));
+        connections.add(UtilityMessageChooserTest.createConnection(this.attachedHost, neighbor2));
+        Collection<Tuple<Message, Connection>> messages =
+                this.chooser.findOtherMessages(Collections.singletonList(m), connections);
+
+        // Make sure the known message was not returned.
+        Assert.assertEquals("Expected different number of chosen messages.", 1, messages.size());
+        Assert.assertFalse(
+                "Known message should not have been returned.",
+                this.messageToHostsExists(messages, m.getId(), neighbor1));
+        Assert.assertTrue(
+                "Message to second neighbor expected.", this.messageToHostsExists(messages, m.getId(), neighbor2));
     }
 
     /**
@@ -297,6 +346,84 @@ public class UtilityMessageChooserTest {
      */
     @Test
     public void testFindOtherMessagesDoesNotReturnMessagesForTransferringRouter() {
+        // Make sure neighbor 1 is transferring to neighbor 2.
+        Message directMessage = new BroadcastMessage(this.neighbor1, "M1", 0);
+        this.neighbor1.createNewMessage(directMessage);
+        this.neighbor1.forceConnection(neighbor2, null, true);
+        this.neighbor1.update(true);
+        this.neighbor2.update(true);
+
+        // Take a look at a non-transferring host for verification.
+        DTNHost otherHost = this.utils.createHost();
+
+        // Give a message to our host.
+        Message m = new Message(this.attachedHost, this.utils.createHost(), "M1", 0);
+        this.attachedHost.createNewMessage(m);
+
+        // Call findOtherMessages with connections to all other three hosts.
+        List<Connection> connections = new ArrayList<>();
+        connections.add(UtilityMessageChooserTest.createConnection(this.attachedHost, neighbor1));
+        connections.add(UtilityMessageChooserTest.createConnection(this.attachedHost, neighbor2));
+        connections.add(UtilityMessageChooserTest.createConnection(this.attachedHost, otherHost));
+        Collection<Tuple<Message, Connection>> messages =
+                this.chooser.findOtherMessages(Collections.singletonList(m), connections);
+
+        // Make sure only the non-transferring host got the message.
+        Assert.assertEquals("Expected different number of chosen messages.", 1, messages.size());
+        Assert.assertFalse(
+                "Host which initiated a transfer should not get messages.",
+                this.messageToHostsExists(messages, m.getId(), neighbor1));
+        Assert.assertFalse(
+                "Host in a transfer should not get messages.",
+                this.messageToHostsExists(messages, m.getId(), neighbor2));
+        Assert.assertTrue(
+                "Message to other neighbor expected.", this.messageToHostsExists(messages, m.getId(), otherHost));
+    }
+
+    @Test
+    public void testDeliveryPredictabilityInfluencesMessageChoosing() {
+        // Prepare two messages with high replications density (shouldn't be sent on that alone).
+        Message lowDeliveryPredMessage = new Message(this.attachedHost, this.utils.createHost(), "M1", 0);
+        Message highDeliveryPredMessage = new Message(this.attachedHost, this.neighbor2, "M2", 0);
+        this.createMessagesWithHighReplicationsDensity(lowDeliveryPredMessage, highDeliveryPredMessage);
+
+        // Make sure neighbor 1 knows the receiver of one of the messages.
+        this.neighbor1.forceConnection(highDeliveryPredMessage.getTo(), null, true);
+
+        // Call findOtherMessages with the messages to neighbor 1.
+        List<Connection> connections = new ArrayList<>();
+        connections.add(UtilityMessageChooserTest.createConnection(this.attachedHost, this.neighbor1));
+        Collection<Tuple<Message, Connection>> messages = this.chooser.findOtherMessages(
+                Arrays.asList(lowDeliveryPredMessage, highDeliveryPredMessage), connections);
+
+        // Check only the one whose receiver neighbor 1 knows is returned.
+        Assert.assertEquals("Expected different number of chosen messages.", 1, messages.size());
+        Assert.assertFalse(
+                "Popular message with no chance of delivery should not be returned.",
+                this.messageToHostsExists(messages, lowDeliveryPredMessage.getId(), this.neighbor1));
+        Assert.assertTrue(
+                "Message with high delivery predictability should be returned.",
+                this.messageToHostsExists(messages, highDeliveryPredMessage.getId(), this.neighbor1));
+
+    }
+
+    @Test
+    public void testRemainingPowerInfluencesMessageChoosing() {
+
+    }
+
+    @Test
+    public void testReplicationsDensityInfluencesMessageChoosing() {
+
+    }
+
+    @Test
+    public void testEncounterValueRatioInfluencesMessageChoosing() {
+
+    }
+
+    @Test
+    public void testUtilityThresholdInfluencesMessageChoosing() {
 
     }
 
@@ -326,5 +453,16 @@ public class UtilityMessageChooserTest {
             }
         }
         return messageFound;
+    }
+
+    private void createMessagesWithHighReplicationsDensity(Message... messages) {
+        DTNHost hostKnowningMessages = this.utils.createHost();
+        for (Message m : messages) {
+            this.attachedHost.createNewMessage(m);
+            hostKnowningMessages.createNewMessage(m);
+        }
+        this.attachedHost.forceConnection(hostKnowningMessages, null, true);
+        this.clock.advance(DisasterRouterTestUtils.RD_WINDOW_LENGTH);
+        this.attachedHost.update(true);
     }
 }

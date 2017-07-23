da38101fec1f4165bf7a36ba90e9bc2b4607701c
Britta Heymann <britta_hey@web.de>
2017-07-23 21:24:18 +0200

    Add tests to DatabaseApplicationUtilTest
    refs #445

--- src/test/DatabaseApplicationUtilTest.java
+++ src/test/DatabaseApplicationUtilTest.java
@@ -8,8 +8,10 @@ import core.DTNHost;
 import core.DataMessage;
 import core.DisasterData;
 import core.Message;
+import core.SimClock;
 import input.DisasterDataNotifier;
 import junit.framework.TestCase;
+import org.junit.After;
 import org.junit.Test;
 import routing.EpidemicRouter;
 import routing.MessageRouter;
@@ -37,8 +39,22 @@ public class DatabaseApplicationUtilTest {
     /** The maximum number of database items per message. */
     private static final int ITEMS_PER_MESSAGE = 2;
 
+    /* Some more constants needed for tests. */
+    private static final Coord FAR_AWAY = new Coord(10_000, 20_000);
+    private static final int POSITIVE_TIMESPAN = 100;
+
+    private static final String UNEXPECTED_NUMBER_MESSAGES = "Expected different number of data messages.";
+    private static final String UNEXPECTED_RECEIVER = "Expected different receiver.";
+    private static final String EXPECTED_EMPTY_LIST = "No data messages should have been returned.";
+
     private TestSettings testSettings = new TestSettings();
     private TestUtils utils = new TestUtils(new ArrayList<>(), new ArrayList<>(), this.testSettings);
+    private SimClock clock = SimClock.getInstance();
+
+    @After
+    public void cleanUp() {
+        SimClock.reset();
+    }
 
     @Test
     public void testHasNoMessagesToSendReturnsFalseIfDatabaseApplicationExists() {
@@ -76,7 +92,7 @@ public class DatabaseApplicationUtilTest {
 
         List<Tuple<Message, Connection>> dataMessages =
                 DatabaseApplicationUtil.wrapUsefulDataIntoMessages(host.getRouter(), host, host.getConnections());
-        TestCase.assertEquals("No data messages should have been returned.", 0, dataMessages.size());
+        TestCase.assertEquals(EXPECTED_EMPTY_LIST, 0, dataMessages.size());
     }
 
     @Test
@@ -87,7 +103,7 @@ public class DatabaseApplicationUtilTest {
 
         List<Tuple<Message, Connection>> dataMessages =
                 DatabaseApplicationUtil.wrapUsefulDataIntoMessages(host.getRouter(), host, host.getConnections());
-        TestCase.assertEquals("No data messages should have been returned.", 0, dataMessages.size());
+        TestCase.assertEquals(EXPECTED_EMPTY_LIST, 0, dataMessages.size());
     }
 
     @Test
@@ -119,12 +135,12 @@ public class DatabaseApplicationUtilTest {
 
         List<Tuple<Message, Connection>> dataMessages =
                 DatabaseApplicationUtil.wrapUsefulDataIntoMessages(host.getRouter(), host, host.getConnections());
-        TestCase.assertEquals("Expected different number of data messages.", TWO_MESSAGES, dataMessages.size());
+        TestCase.assertEquals(UNEXPECTED_NUMBER_MESSAGES, TWO_MESSAGES, dataMessages.size());
 
         DataMessage message1 = (DataMessage)dataMessages.get(0).getKey();
         DataMessage message2 = (DataMessage)dataMessages.get(1).getKey();
-        TestCase.assertEquals("Expected different receiver.", message1.getTo(), neighbor1);
-        TestCase.assertEquals("Expected different receiver.", message2.getTo(), neighbor2);
+        TestCase.assertEquals(UNEXPECTED_RECEIVER, message1.getTo(), neighbor1);
+        TestCase.assertEquals(UNEXPECTED_RECEIVER, message2.getTo(), neighbor2);
         TestCase.assertEquals("Data should be the same for both messages.", message1.getData(), message2.getData());
         TestCase.assertEquals(
                 "Utility should be the same for both messages.", message1.getUtility(), message2.getUtility());
@@ -148,11 +164,97 @@ public class DatabaseApplicationUtilTest {
 
         List<Tuple<Message, Connection>> dataMessages =
                 DatabaseApplicationUtil.wrapUsefulDataIntoMessages(host.getRouter(), host, host.getConnections());
-        TestCase.assertEquals("Expected different number of data messages.", 1, dataMessages.size());
+        TestCase.assertEquals(UNEXPECTED_NUMBER_MESSAGES, 1, dataMessages.size());
         DataMessage message = (DataMessage)dataMessages.get(0).getKey();
         TestCase.assertEquals("Expected different number of data items.", ITEMS_PER_MESSAGE, message.getData().size());
         TestCase.assertTrue("Marker should have been returned.", message.getData().contains(MARKER_DATA));
         TestCase.assertTrue("Resource should have been returned.", message.getData().contains(RESOURCE_DATA));
+    }
+
+    @Test
+    public void testWrapRecentUsefulDataIntoMessagesReturnsEmptyListForNoDatabaseApplication() {
+        DTNHost host = this.utils.createHost();
+        DisasterDataNotifier.dataCreated(host, MARKER_DATA);
+
+        DTNHost connected = this.utils.createHost();
+        host.connect(connected);
+
+        List<Tuple<Message, Connection>> dataMessages =
+                DatabaseApplicationUtil.wrapRecentUsefulDataIntoMessages(host, host.getConnections(), 1);
+        TestCase.assertEquals(EXPECTED_EMPTY_LIST, 0, dataMessages.size());
+    }
+
+    @Test
+    public void testWrapRecentUsefulDataIntoMessagesReturnsEmptyListIfNoConnectionsAvailable() {
+        this.addDatabaseApplication();
+        DTNHost host = this.utils.createHost();
+        DisasterDataNotifier.dataCreated(host, MARKER_DATA);
+
+        List<Tuple<Message, Connection>> dataMessages =
+                DatabaseApplicationUtil.wrapRecentUsefulDataIntoMessages(host, host.getConnections(), 1);
+        TestCase.assertEquals(EXPECTED_EMPTY_LIST, 0, dataMessages.size());
+    }
+
+    @Test
+    public void testWrapRecentUsefulDataIntoMessagesReturnsEachMessageForEachNeighbor() {
+        this.addDatabaseApplication();
+        DTNHost host = this.utils.createHost();
+        DisasterDataNotifier.dataCreated(host, MARKER_DATA);
+
+        DTNHost neighbor1 = this.utils.createHost();
+        DTNHost neighbor2 = this.utils.createHost();
+        host.connect(neighbor1);
+        host.connect(neighbor2);
+
+        List<Tuple<Message, Connection>> dataMessages =
+                DatabaseApplicationUtil.wrapRecentUsefulDataIntoMessages(host, host.getConnections(), 1);
+        TestCase.assertEquals(UNEXPECTED_NUMBER_MESSAGES, TWO_MESSAGES, dataMessages.size());
+
+        DataMessage message1 = (DataMessage)dataMessages.get(0).getKey();
+        DataMessage message2 = (DataMessage)dataMessages.get(1).getKey();
+        TestCase.assertEquals(UNEXPECTED_RECEIVER, message1.getTo(), neighbor1);
+        TestCase.assertEquals(UNEXPECTED_RECEIVER, message2.getTo(), neighbor2);
+        TestCase.assertEquals("Data should be the same for both messages.", message1.getData(), message2.getData());
+        TestCase.assertEquals(
+                "Utility should be the same for both messages.", message1.getUtility(), message2.getUtility());
+        TestCase.assertEquals(
+                "Creation should be the same for both messages.",
+                message1.getCreationTime(),
+                message2.getCreationTime());
+        TestCase.assertEquals("Size should be the same for both messages", message1.getSize(), message2.getSize());
+        TestCase.assertEquals("Sender should be the same for both messages", message1.getFrom(), message2.getFrom());
+    }
+
+    @Test
+    public void testWrapRecentUsefulDataIntoMessagesReturnsRecentInterestingData() {
+        // Prepare a setting where it's easy to provide old and useless data.
+        this.addDatabaseApplication();
+        this.clock.setTime(POSITIVE_TIMESPAN);
+        DTNHost host = this.utils.createHost();
+        host.setLocation(FAR_AWAY);
+
+        // Prepare three data items: One useful, but not recent; one recent, but not useful; and one useful and recent.
+        DisasterData notRecent = new DisasterData(DisasterData.DataType.MARKER, 0, 0, host.getLocation());
+        DisasterData notUseful = new DisasterData(DisasterData.DataType.MARKER, 0, SimClock.getTime(), new Coord(0, 0));
+        DisasterData recentAndUseful =
+                new DisasterData(DisasterData.DataType.MARKER, 0, SimClock.getTime(), host.getLocation());
+        DisasterDataNotifier.dataCreated(host, notRecent);
+        DisasterDataNotifier.dataCreated(host, notUseful);
+        DisasterDataNotifier.dataCreated(host, recentAndUseful);
+
+        // Meet with neighbor.
+        DTNHost neighbor = this.utils.createHost();
+        host.connect(neighbor);
+
+        // Check only useful and recent item is returned.
+        List<Tuple<Message, Connection>> dataMessages =
+                DatabaseApplicationUtil.wrapRecentUsefulDataIntoMessages(host, host.getConnections(), 1);
+        TestCase.assertEquals(UNEXPECTED_NUMBER_MESSAGES, 1, dataMessages.size());
+        DataMessage message = (DataMessage)dataMessages.get(0).getKey();
+        TestCase.assertFalse("Old data item should not have been returned.", message.getData().contains(notRecent));
+        TestCase.assertFalse("Useless data item should not have been returned.", message.getData().contains(notUseful));
+        TestCase.assertTrue("Expected recent useful data item to be in message.",
+                message.getData().contains(recentAndUseful));
     }
 
     private void addDatabaseApplication() {
@@ -165,7 +267,7 @@ public class DatabaseApplicationUtilTest {
     }
 
     private static void addDummyValuesForDatabaseApplication(TestSettings s) {
-        s.putSetting(DatabaseApplication.UTILITY_THRESHOLD, "0.0");
+        s.putSetting(DatabaseApplication.UTILITY_THRESHOLD, "0.1");
         s.putSetting(DatabaseApplication.SIZE_RANDOMIZER_SEED, "0");
         s.putSetting(DatabaseApplication.DATABASE_SIZE_RANGE, "0,0");
         s.putSetting(DatabaseApplication.MIN_INTERVAL_MAP_SENDING, "0");

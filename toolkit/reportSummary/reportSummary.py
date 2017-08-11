4db379ac657b365dbbafd83baaaf5c7688a558e2
Melanie Bruns <mbruns42@mail.upb.de>
2017-08-11 15:27:04 +0200

    Add other two scripts as described in "how to evaluate routing .."

--- toolkit/reportSummary/reportSummary.py
+++ toolkit/reportSummary/reportSummary.py
@@ -4,3 +4,10 @@ with open('../../reports/messageDelayAnalysis.txt', 'w', 1) as file:
     sh.perl("../messageDelayAnalyzer.pl", "../../reports/realisticScenario_ImmediateMessageDelayReport.txt", "60",
             _out=file)
 
+with open('../../reports/broadcastMessageAnalysis.txt', 'w', 1) as file:
+    sh.perl("../broadcastMessageAnalyzer.pl", "../../reports/realisticScenario_BroadcastDeliveryReport.txt", "60",
+            _out=file)
+
+with open('../../reports/multicastMessageAnalysis.txt', 'w', 1) as file:
+    sh.perl("../multicastMessageAnalyzer.pl", "../../reports/realisticScenario_MulticastMessageDeliveryReport.txt", "60",
+            _out=file)

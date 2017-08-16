371fc57c6b561e453c1073decf0fb4f6291f8617
Melanie Bruns <mbruns42@mail.upb.de>
2017-08-16 16:34:43 +0200

    Increase interval to five minutes

--- toolkit/reportSummary/reportSummary.py
+++ toolkit/reportSummary/reportSummary.py
@@ -1,13 +1,13 @@
 import sh
 
 with open('../../reports/messageDelayAnalysis.txt', 'w', 1) as file:
-    sh.perl("../messageDelayAnalyzer.pl", "../../reports/realisticScenario_ImmediateMessageDelayReport.txt", "60",
+    sh.perl("../messageDelayAnalyzer.pl", "../../reports/realisticScenario_ImmediateMessageDelayReport.txt", "300",
             _out=file)
 
 with open('../../reports/broadcastMessageAnalysis.txt', 'w', 1) as file:
-    sh.perl("../broadcastMessageAnalyzer.pl", "../../reports/realisticScenario_BroadcastDeliveryReport.txt", "60",
+    sh.perl("../broadcastMessageAnalyzer.pl", "../../reports/realisticScenario_BroadcastDeliveryReport.txt", "300",
             _out=file)
 
 with open('../../reports/multicastMessageAnalysis.txt', 'w', 1) as file:
-    sh.perl("../multicastMessageAnalyzer.pl", "../../reports/realisticScenario_MulticastMessageDeliveryReport.txt", "60",
+    sh.perl("../multicastMessageAnalyzer.pl", "../../reports/realisticScenario_MulticastMessageDeliveryReport.txt", "300",
             _out=file)

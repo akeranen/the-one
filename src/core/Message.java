cf5b484e95fce6c20ba87f723151f443f9e2e2e2
Melanie Bruns <mbruns42@mail.upb.de>
2017-08-17 16:35:47 +0200

    Add method in message to check whether TTL is over

--- src/core/Message.java
+++ src/core/Message.java
@@ -534,4 +534,9 @@ public class Message implements Comparable<Message> {
         return this.priority;
     }
 
+    public boolean isTimeToLiveOver(){
+        final int ONE_MIN_IN_S = 60;
+        return (timeCreated + initTtl*ONE_MIN_IN_S < SimClock.getIntTime());
+    }
+
 }

e7ab012f10f48a848a77b8ac7947e31185e752b8
Melanie Bruns <mbruns42@mail.upb.de>
2017-08-17 13:09:00 +0200

    Take report directory and seeds as command line arguments

--- toolkit/reportSummary/reportSummary.py
+++ toolkit/reportSummary/reportSummary.py
@@ -1,13 +1,56 @@
 from subprocess import run
+import sys
+import getopt
 
-with open('../../reports/messageDelayAnalysis.txt', 'w', 1) as file:
-    run("perl ../messageDelayAnalyzer.pl ../../reports/realisticScenario_ImmediateMessageDelayReport.txt 300",
-        stdout=file)
+#ReportSummary:
+#
+#Usage: reportSummary.py -r <reportDirectory>
+#
+# 1. Calls all pre-processing perl script on a given report directory
+#    The shortened reports are placed within the reports directory
+# 2. TODO: Calls visualization scripts and places the results within a
+#    subdirectory called images
+# 3. TODO: Outputs all images into pdf in consistent order (calls another script that does)
 
-with open('../../reports/broadcastMessageAnalysis.txt', 'w', 1) as file:
-    run("perl ../broadcastMessageAnalyzer.pl ../../reports/realisticScenario_BroadcastDeliveryReport.txt 300",
-        stdout=file)
 
-with open('../../reports/multicastMessageAnalysis.txt', 'w', 1) as file:
-    run("perl ../multicastMessageAnalyzer.pl ../../reports/realisticScenario_MulticastMessageDeliveryReport.txt 300",
-        stdout=file)
+## Retrieve command line options
+relevantOptions = sys.argv[1:]
+#Default values for options
+reportDir = 'reports/'
+seeds = []
+# Try to retrieve command line parameters
+try:
+    opts, args = getopt.getopt(relevantOptions,"r:s:")
+except getopt.GetoptError:
+    print('Usage: reportSummary.py -r <reportDirectory> -s <seeds>\n'
+          'e.g., -r /Simulator/reports/ -s 1,2,3')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == "-r":
+        reportDir = arg
+    elif opt == "-s":
+        seeds = arg.split(',')
+
+## Call perl scripts for report pre-processing
+#Find out what runs you need to do depending on seeds
+granularity = "300"
+perlNames = [["messageDelayAnalyzer.pl", "realisticScenario_ImmediateMessageDelayReport", "messageDelayAnalysis"],
+             ["broadcastMessageAnalyzer.pl", "realisticScenario_BroadcastDeliveryReport", "broadcastMessageAnalysis"],
+             ["messageDelayAnalyzer.pl", "realisticScenario_ImmediateMessageDelayReport", "multicastMessageAnalysis"]]
+
+necessaryAnalyses = []
+if len(seeds) == 0:
+    for (script, input, output) in perlNames:
+        necessaryAnalyses.append([script, reportDir+input+".txt", reportDir+output+".txt"])
+else:
+    for seed in seeds:
+        for (script, input, output) in perlNames:
+            necessaryAnalyses.append([script, reportDir+input+"_"+seed+".txt", reportDir+output+"_"+seed+".txt"])
+
+print(necessaryAnalyses)
+
+#Execute script with input and print to output
+for (script, input, output) in necessaryAnalyses:
+    with open(output, 'w', 1) as file:
+        run("perl "+ script + " "+ input + " " + granularity, stdout=file)
+

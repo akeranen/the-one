970da74c61b0a7abdd6d66586ee44b3f5dfb4342
Melanie Bruns <mbruns42@mail.upb.de>
2017-08-18 09:54:01 +0200

    Correct name of Multicast Delivery report

--- toolkit/reportSummary/reportSummary.py
+++ toolkit/reportSummary/reportSummary.py
@@ -37,7 +37,7 @@ for opt, arg in opts:
 granularity = "300"
 perlNames = [["messageDelayAnalyzer.pl", "realisticScenario_ImmediateMessageDelayReport", "messageDelayAnalysis"],
              ["broadcastMessageAnalyzer.pl", "realisticScenario_BroadcastDeliveryReport", "broadcastMessageAnalysis"],
-             ["multicastMessageAnalyzer.pl", "realisticScenario_ImmediateMessageDelayReport", "multicastMessageAnalysis"]]
+             ["multicastMessageAnalyzer.pl", "realisticScenario_MulticastMessageDeliveryReport", "multicastMessageAnalysis"]]
 
 necessaryAnalyses = []
 if len(seeds) == 0:

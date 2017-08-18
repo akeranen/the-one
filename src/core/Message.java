ed672e70a19ddf7aa49ed311ad61d1c1ef708d6c
Melanie Bruns <mbruns42@mail.upb.de>
2017-08-18 10:31:12 +0200

    Revert "Add method in message to check whether TTL is over"
    This reverts commit cf5b484

--- src/core/Message.java
+++ src/core/Message.java
@@ -534,9 +534,4 @@ public class Message implements Comparable<Message> {
         return this.priority;
     }
 
-    public boolean isTimeToLiveOver(){
-        final int ONE_MIN_IN_S = 60;
-        return (timeCreated + initTtl*ONE_MIN_IN_S < SimClock.getIntTime());
-    }
-
 }

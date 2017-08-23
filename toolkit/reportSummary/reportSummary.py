fc6e5c117cd6e7afb969164503d25a611bac296c
Britta Heymann <britta_hey@web.de>
2017-08-23 16:33:48 +0200

    Change report summary to call all scripts
    refs #515

--- toolkit/reportSummary/reportSummary.py
+++ toolkit/reportSummary/reportSummary.py
@@ -1,6 +1,17 @@
 import subprocess
 import sys
 import getopt
+import os
+
+import trafficAnalysis
+import privateMessageAnalysis
+import delayDistributionAnalysis
+import broadcastAnalysis
+import multicastAnalysis
+import dataSyncAnalysis
+import reportSummaryPdf
+
+import readFileUtilities
 
 #ReportSummary:
 #
@@ -9,9 +20,9 @@ import getopt
 #
 # 1. Calls all pre-processing perl script on a given report directory
 #    The shortened reports are placed within the reports directory
-# 2. TODO: Calls visualization scripts and places the results within a
+# 2. Calls visualization scripts and places the results within a
 #    subdirectory called images
-# 3. TODO: Outputs all images into pdf in consistent order (calls another script that does)
+# 3. Outputs all images into pdf in consistent order (calls another script that does)
 
 
 ## Retrieve command line options
@@ -51,7 +62,49 @@ else:
 #Execute script with input and write to output
 for (script, input, output) in necessaryAnalyses:
     with open(output, 'w', 1) as file:
-        process = subprocess.run("perl "+ script + " "+ input + " " + granularity)
-        file.write(process.stdout)
+        process = subprocess.run("perl "+ script + " "+ input + " " + granularity, stdout=file)
         print("Successfully created ", output)
 
+# Create images/ directory in reports/ if it does not exist yet
+imageDirectoryName = reportDir + 'images/'
+if not os.path.exists(imageDirectoryName):
+    os.makedirs(imageDirectoryName)
+print("Made sure directory exists: ", imageDirectoryName)
+
+# Call all visualization scripts
+trafficAnalysis.main(
+    analysisFileName=reportDir+"realisticScenario_TrafficReport.txt",
+    graphicFileName=readFileUtilities.getAbsoluteTrafficAnalysisPath(imageDirectoryName))
+privateMessageAnalysis.main(
+    analysisFileName=reportDir+"realisticScenario_DeliveryProbabilityReport.txt",
+    graphicFileName=readFileUtilities.getAbsoluteDeliveryRatePath(imageDirectoryName))
+delayDistributionAnalysis.main(
+    analysisFileName=reportDir+"messageDelayAnalysis.txt",
+    messageType="ONE_TO_ONE",
+    messagePrio=0,
+    graphicFileName=readFileUtilities.getAbsoluteOneToOneMessageDelayPath(imageDirectoryName))
+broadcastAnalysis.main(
+    analysisFileName=reportDir+"broadcastMessageAnalysis.txt",
+    graphicFileName=readFileUtilities.getAbsoluteBroadcastAnalysisPath(imageDirectoryName))
+relevantPriorities = [2, 5, 9]
+for prio in relevantPriorities:
+    delayDistributionAnalysis.main(
+        analysisFileName=reportDir+"messageDelayAnalysis.txt",
+        messageType="BROADCAST",
+        messagePrio=prio,
+        graphicFileName=readFileUtilities.getAbsoluteBroadcastDelayPath(imageDirectoryName, prio))
+multicastAnalysis.main(
+    analysisFileName=reportDir+"multicastMessageAnalysis.txt",
+    graphicFileName=readFileUtilities.getAbsoluteMulticastAnalysisPath(imageDirectoryName))
+delayDistributionAnalysis.main(
+    analysisFileName=reportDir+"messageDelayAnalysis.txt",
+    messageType="MULTICAST",
+    messagePrio=1,
+    graphicFileName=readFileUtilities.getAbsoluteMulticastDelayPath(imageDirectoryName))
+dataSyncAnalysis.main(
+    analysisFileName=reportDir+"realisticScenario_DataSyncReport.txt",
+    graphicFileName=readFileUtilities.getAbsoluteDataAnalysisPath(imageDirectoryName))
+
+print("Successfully created all graphics. Creating pdf...")
+
+reportSummaryPdf.main(imageDirectoryName)

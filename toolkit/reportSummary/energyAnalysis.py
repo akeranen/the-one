9ccf77beeada25127fd16f3e6df6ea2e340b6a51
Robin Oppermann <robinopp@mail.uni-paderborn.de>
2017-09-07 10:16:33 +0200

    Add evaluation of total number of nodes that ever reached the energy limits to the energyAnalysis script

--- toolkit/reportSummary/energyAnalysis.py
+++ toolkit/reportSummary/energyAnalysis.py
@@ -25,6 +25,8 @@ class EnergyData:
     timePoints = []
     zeroEnergyHosts = []
     lowEnergyHosts = []
+    nodesThatEverReachedLowEnergyLimit = []
+    nodesThatEverReachedZeroEnergy = []
 
     # Current counters
     currentTimePoint = 0
@@ -32,11 +34,20 @@ class EnergyData:
     currentZeroEnergyHosts = 0
     currentLowEnergyHosts = 0
 
+    # Sets of nodes that ever reached the limit
+    currentNodesThatEverReachedLowEnergyLimit = set()
+    currentNodesThatEverReachedZeroEnergy = set()
+    allNodesInTheSimulation = set()
+
     # Transfers current counters into new elements of the summarizing data vectors.
     def updateData(self):
         self.timePoints.append(self.currentTimePoint)
         self.zeroEnergyHosts.append(self.currentZeroEnergyHosts / self.currentTotalHosts)
         self.lowEnergyHosts.append(self.currentLowEnergyHosts / self.currentTotalHosts)
+        self.nodesThatEverReachedLowEnergyLimit\
+            .append(len(self.currentNodesThatEverReachedLowEnergyLimit) / len(self.allNodesInTheSimulation))
+        self.nodesThatEverReachedZeroEnergy \
+            .append(len(self.currentNodesThatEverReachedZeroEnergy) / len(self.allNodesInTheSimulation))
 
     # Resets current counters to 0.
     def resetCounters(self):
@@ -52,20 +63,32 @@ class EnergyData:
         elif energyLevel < 0.1:
             self.currentLowEnergyHosts += 1
 
+    def updateNodeSets(self, node, energyLevel):
+        self.allNodesInTheSimulation.add(node)
+        if energyLevel < 0.1:
+            self.currentNodesThatEverReachedLowEnergyLimit.add(node)
+            if energyLevel == 0:
+                self.currentNodesThatEverReachedZeroEnergy.add(node)
+
 # Draws two functions over the same x values.
 # Labels are selected as appropriate for energy analysis.
-def drawPlots(x, y_lowEnergy, y_noEnergy):
+def drawPlots(x, y_lowEnergy, y_noEnergy, y_totalLowEnergy, y_totalNoEnergy, graphicFileName):
     plt.title('Battery power distribution')
     plt.xlabel('Minutes in simulation')
     plt.ylabel('Percentage of hosts')
-    plt.plot(x, y_lowEnergy, '.-', label='Battery 0% < x < 10%')
-    plt.plot(x, y_noEnergy, '.-',  label='No battery left')
-    plt.legend(loc='upper left')
+    plt.plot(x, y_lowEnergy, '.-', label='Current nodes with battery 0% < x < 10%')
+    plt.plot(x, y_totalLowEnergy, '.-', label='Total nodes that reached battery 0% < x < 10%')
+    plt.plot(x, y_noEnergy, '.-',  label='Current nodes with no battery left')
+    plt.plot(x, y_totalNoEnergy, '.-',  label='Total nodes that had no battery left')
+    legend = plt.legend(bbox_to_anchor=(0.1, -0.15), loc=2, borderaxespad=0.)
     plt.grid(True)
     axes = plt.gca()
     axes.set_ylim(ymin=0,ymax=1)
     axes.set_xlim(xmin=0)
     axes.xaxis.set_major_locator(ticker.IndexLocator(base=60, offset=-10))
+    # Save to file
+    plt.savefig(graphicFileName, bbox_extra_artists=(legend,), bbox_inches='tight')
+    plt.close()
 
 # Main function of the script. See script description at the top of the file for further information.
 def main(analysisFileName, graphicFileName):
@@ -77,7 +100,7 @@ def main(analysisFileName, graphicFileName):
     data = EnergyData()
     for line in analysis:
         timeMatch = re.match("\[(\d+)\]", line)
-        energyMatch = re.match("\d+,\D+\d+,(\d+.\d+)", line)
+        energyMatch = re.match("\d+,(\D+\d+),(\d+.\d+)", line)
         # If the line looks like "[600]", i.e. the report changes to the next time frame, we have to finalize the
         # data we collected for the last time point (if there was such) and then reset our counters.
         if timeMatch is not None:
@@ -88,13 +111,14 @@ def main(analysisFileName, graphicFileName):
         # Otherwise, if the line looks like '600,p0,0.8116', it is another data point for the current time and we
         # should update our counters using the logged energy.
         if energyMatch is not None:
-            data.updateCounters(energyLevel=float(energyMatch.group(1)))
+            data.updateCounters(energyLevel=float(energyMatch.group(2)))
+            data.updateNodeSets(energyLevel=float(energyMatch.group(2)), node=energyMatch.group(1))
 
-    drawPlots(x=data.timePoints, y_lowEnergy=data.lowEnergyHosts, y_noEnergy=data.zeroEnergyHosts)
-
-    # Save to file
-    plt.savefig(graphicFileName)
-    plt.close()
+    drawPlots(x=data.timePoints,
+              y_lowEnergy=data.lowEnergyHosts,
+              y_noEnergy=data.zeroEnergyHosts,
+              y_totalLowEnergy=data.nodesThatEverReachedLowEnergyLimit,
+              y_totalNoEnergy=data.nodesThatEverReachedZeroEnergy, graphicFileName=graphicFileName)
 
 # Make sure script can be called from command line.
 if __name__ == "__main__":

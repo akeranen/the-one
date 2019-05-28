e0f0d9f8e97ed01c39aa628b88105aa36a7495dc
Jakob Mass <jaks@ut.ee>
2019-05-28 12:01:26 +0300

    Add new initial location types for LinearMovement. Namely: * Added option for locating the node at the beginning of movement line. * Added option to evenly space nodes along a specified subsegment of movement line.

--- src/movement/LinearMovement.java
+++ src/movement/LinearMovement.java
@@ -6,8 +6,6 @@ package movement;
 
 import core.Coord;
 import core.Settings;
-import movement.MovementModel;
-import movement.Path;
 
 /**
  * Movement model where all nodes move on a line
@@ -27,8 +25,11 @@ public class LinearMovement extends MovementModel {
 	 * Nodes' initial location type
 	 * <ul><li>0: random (evenly distributed)
 	 * <li>1: evenly spaced
+	 * <li>2: static location at the start of line
+	 * <li>3: evenly spaced among subsegment (needs startSegmentEndLocation)
 	 * </ul>
 	  */
+	public static final String INIT_LOC_SEGMENT_END = "initLocSegmentEnd";
 	public static final String INIT_LOC_S = "initLocType";
 	/**
 	 * Nodes' target (where they're heading) type
@@ -37,6 +38,7 @@ public class LinearMovement extends MovementModel {
 	 * </ul>
 	  */
 	public static final String TARGET_S = "targetType";
+	private Coord initLocSegmentEnd; /** Used for node initial location distribution among segment*/
 
 	/* values for the prototype */
 	private Coord startLoc; /** The start location of the line */
@@ -62,6 +64,10 @@ public class LinearMovement extends MovementModel {
 		this.startLoc = new Coord(coords[0], coords[1]);
 		coords = s.getCsvInts(LINEAR_MOVEMENT_NS + END_LOCATION_S, 2);
 		this.endLoc = new Coord(coords[0], coords[1]);
+		if (s.contains(LINEAR_MOVEMENT_NS + INIT_LOC_SEGMENT_END)){
+			coords = s.getCsvInts(LINEAR_MOVEMENT_NS + INIT_LOC_SEGMENT_END);
+			this.initLocSegmentEnd = new Coord(coords[0], coords[1]);
+		}
 		this.initLocType = s.getInt(LINEAR_MOVEMENT_NS + INIT_LOC_S);
 		this.targetType = s.getInt(LINEAR_MOVEMENT_NS + TARGET_S);
 		this.nodeCount = s.getInt(core.SimScenario.NROF_HOSTS_S);
@@ -75,7 +81,12 @@ public class LinearMovement extends MovementModel {
 	 */
 	public LinearMovement(LinearMovement ilm) {
 		super(ilm);
-		this.initLoc = calculateLocation(ilm, (ilm.initLocType == 1));
+		if (ilm.initLocType == 2)
+			this.initLoc = ilm.startLoc;
+		else if (ilm.initLocType == 3)
+			this.initLoc = calculateLocationForSegment(ilm);
+		else
+			this.initLoc = calculateLocation(ilm, (ilm.initLocType == 1));
 		this.nextPath = new Path(generateSpeed());
 		this.nextPath.addWaypoint(initLoc);
 
@@ -86,6 +97,25 @@ public class LinearMovement extends MovementModel {
 		}
 
 		ilm.lastIndex++;
+	}
+
+	/** Spreads locations out evenly among the segment defined by initLocSegmentEnd (initLocType 3)*/
+	private Coord calculateLocationForSegment(LinearMovement proto) {
+		double dx = 0;
+		double dy = 0;
+		double placementFraction;
+
+		double xDiff = (proto.initLocSegmentEnd.getX() -  proto.startLoc.getX());
+		double yDiff = (proto.initLocSegmentEnd.getY() -  proto.startLoc.getY());
+		Coord c = proto.startLoc.clone();
+
+
+		placementFraction = (1.0 * proto.lastIndex / proto.nodeCount);
+		dx = placementFraction * xDiff;
+		dy = placementFraction * yDiff;
+
+		c.translate(dx, dy);
+		return c;
 	}
 
 	/**

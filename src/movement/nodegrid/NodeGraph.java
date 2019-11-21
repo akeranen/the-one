addad19fea5c5433bf495df29cf0c648ca79bd04
Felix Schorer <felixschorer@users.noreply.github.com>
2019-11-21 20:15:05 +0100

    Add TODOs for coordination

--- src/movement/nodegrid/NodeGraph.java
+++ src/movement/nodegrid/NodeGraph.java
@@ -8,28 +8,61 @@ import java.util.*;
 
 public class NodeGraph extends SimMap {
 
+    @Deprecated
     public NodeGraph(Polygon outerBound, double rasterInterval) {
         super(rasterPolygon(outerBound, rasterInterval));
     }
 
-    private static Map<Coord, MapNode> rasterPolygon(Polygon outerBound, double rasterInterval) {
-        Polygon.BoundingBox boundingBox = outerBound.getBoundingBox();
-        Coord anchor = boundingBox.getTopLeft();
+    public NodeGraph(double rasterInterval) {
+        // TODO
+        super(new HashMap<>());
+    }
 
-        double width = boundingBox.getBottomRight().getX() - boundingBox.getTopLeft().getX();
-        double height = boundingBox.getBottomRight().getY() - boundingBox.getTopLeft().getY();
+    /**
+     * TODO
+     * Raster polygon into hexagonal shaped nodes and them to the graph
+     * @param polygon The polygon to add
+     */
+    public void addPolygon(Polygon polygon) {
+    }
+
+    /**
+     * TODO
+     * Remove nodes contained within the polygon form the graph
+     * @param polygon The polygon to subtract
+     */
+    public void subtractPolygon(Polygon polygon) {
+    }
+
+    /**
+     * TODO
+     * Attach nodes by their n closest nodes to the graph
+     * @param numberOfAttachmentPoints The number of attachment points
+     * @param nodes The nodes to attach to the graph
+     */
+    public void attachNodeToClosestNodes(int numberOfAttachmentPoints, MapNode... nodes) {
+    }
+
+    private static Map<Coord, MapNode> rasterPolygon(Polygon outerBound, double rasterInterval) {
+        // hexagon parameters
         double innerRadius = rasterInterval / 2;
         double outerRadius = innerRadius / Math.cos(Math.toRadians(30));
 
+        Polygon.BoundingBox boundingBox = outerBound.getBoundingBox();
+        double width = boundingBox.getBottomRight().getX() - boundingBox.getTopLeft().getX();
+        double height = boundingBox.getBottomRight().getY() - boundingBox.getTopLeft().getY();
+
+        // determine raster size
         int horizontalSteps = (int) Math.floor(width / (innerRadius * 2));
         int verticalSteps = (int) Math.floor(height / (outerRadius * 1.5));
 
         Map<Coord, MapNode> nodes = new HashMap<>();
         MapNode[][] raster = new MapNode[verticalSteps][horizontalSteps];
+        Coord anchor = boundingBox.getTopLeft();
 
         // raster polygon into hexagons
         for (int row = 0; row < verticalSteps; row++) {
-            double horizontalOffset = isOffsetRow(row) ? innerRadius : 0;
+            double horizontalOffset = isRowOffset(row) ? innerRadius : 0;
             double y = anchor.getY() + row * 1.5 * outerRadius;
             for (int column = 0; column < horizontalSteps; column++) {
                 double x = anchor.getX() + horizontalOffset + column * 2 * innerRadius;
@@ -56,18 +89,19 @@ public class NodeGraph extends SimMap {
         return nodes;
     }
 
-    private static boolean isOffsetRow(int row) {
+    private static boolean isRowOffset(int row) {
         return row % 2 != 0;
     }
 
     private static List<MapNode> getAdjacentNodes(MapNode[][] mapNodes, int row, int column) {
         List<MapNode> adjacentNodes = new ArrayList<>();
+
         getNode(mapNodes, row, column - 1).ifPresent(adjacentNodes::add);
         getNode(mapNodes, row, column + 1).ifPresent(adjacentNodes::add);
         getNode(mapNodes, row - 1, column).ifPresent(adjacentNodes::add);
         getNode(mapNodes, row + 1, column).ifPresent(adjacentNodes::add);
 
-        if (isOffsetRow(row)) {
+        if (isRowOffset(row)) {
             getNode(mapNodes, row - 1, column + 1).ifPresent(adjacentNodes::add);
             getNode(mapNodes, row + 1, column + 1).ifPresent(adjacentNodes::add);
         } else {

a10ebf665a1499aa551ae5273ec6c29a69c49b03
crydsch <crydsch@lph.zone>
2023-10-02 17:10:45 +0200

    Refactor MSIM optimizer for LinkUp/LinkDown events

--- src/interfaces/MSIMConnectivityOptimizer.java
+++ src/interfaces/MSIMConnectivityOptimizer.java
@@ -1,10 +1,11 @@
 package interfaces;
 
-import core.DTNHost;
 import core.NetworkInterface;
-import core.Settings;
+import core.SettingsError;
+import input.MSIMConnector;
+import movement.MovementEngine;
 
-import java.util.*;
+import java.util.List;
 
 /**
  * Provides GPU accelerated connectivity detection as an extension to an MSIMMovementEngine.
@@ -13,72 +14,95 @@ import java.util.*;
 public class MSIMConnectivityOptimizer extends ConnectivityOptimizer {
 	/** Class name */
 	public static final String NAME = "MSIMConnectivityOptimizer";
-	/** List of all hosts in the simulation */
-	private List<DTNHost> hosts = null;
-	/** Map of Interfaces within range of each other */
-	private List<Set<NetworkInterface>> interfacesInRange = null;
 
-	/**
-	 * Creates a new MSIMConnectivityOptimizer based on a Settings object's settings.
-	 * @param s The Settings object where the settings are read from
-	 */
-	public MSIMConnectivityOptimizer(Settings s) {}
+	/** Connector for IPC */
+	private MSIMConnector connector = null;
+	/** List of managed interfaces */
+	private List<NetworkInterface> interfaces = null;
 
-	public void init(List<DTNHost> hosts) {
-		this.hosts = hosts;
+	public MSIMConnectivityOptimizer(MSIMConnector connector, List<NetworkInterface> interfaces) {
+		this.connector = connector;
+		this.interfaces = interfaces;
 
-		this.interfacesInRange = new ArrayList<>(hosts.size());
-		for (int i = 0; i < hosts.size(); i++) {
-			this.interfacesInRange.add(i, new LinkedHashSet<>());
+		// Sanity checks
+		for (int i = 0; i < interfaces.size(); i++) {
+			if (interfaces.get(i).getHost().getID() != i) {
+				throw new SettingsError("MSIMConnectivityOptimizer requires one interface the same type for every host!");
+			}
 		}
 	}
 
+	// TODO move to debug_utils class
+	private String toHumanTime(long nanos) {
+		if (nanos > 1000000000) {
+			// at least one second
+			return (nanos / 1000000000) + " s";
+		}
+		if (nanos > 1000000) {
+			// less than a second, but at least one millisecond
+			return (nanos / 1000000) + " ms";
+		}
+		if (nanos > 1000) {
+			// less than a millisecond, but at least one microsecond
+			return (nanos / 1000) + " us";
+		}
+		// less than a microsecond
+		return nanos + " ns";
+	}
+
+	/**
+	 * Detects interfaces which are in range/no longer in range of each other
+	 * Issues LinkUp/LinkDown events to the corresponding interfaces
+	 */
 	@Override
-	public boolean areWithinRange(NetworkInterface a, NetworkInterface b) {
-		int IDa = NI_to_ID(a);
-		int IDb = NI_to_ID(b);
-		int min = Math.min(IDa, IDb);
-		int max = Math.max(IDa, IDb);
-		return interfacesInRange.get(min).contains(ID_to_NI(max));
-	}
+	public void detectConnectivity() {
+		// Run connectivity detection
 
-	/**
-	 * Finds all network interfaces that might be located so that they can be
-	 * connected with the network interface (corresponds to MSIM link up events)
-	 *
-	 * @param ni network interface that needs to be connected
-	 * @return A collection of network interfaces within proximity
-	 */
-	public Collection<NetworkInterface> getInterfacesInRange(NetworkInterface ni) {
-		return interfacesInRange.get(NI_to_ID(ni));
-	}
+		// TODO
+		boolean disableLinkEvents = false;
+		if (disableLinkEvents) {
+			/*// Get connectivity via list of collisions
+			connector.writeHeader(MSIMConnector.Header.CollisionDetection);
+			connector.flushOutput();
 
-	public void resetAllEvents() {
-		for(Set<NetworkInterface> s : interfacesInRange) {
-			s.clear();
+			// Receive collisions
+			int collisionsCount = connector.readInt();
+			for (int i = 0; i < collisionsCount; i++) {
+				int ID0 = connector.readInt();
+				int ID1 = connector.readInt();
+				applyLinkUpEvent(ID0, ID1);
+			}*/
+		} else {
+			// Get connectivity via link up/link down events
+			long startPass = System.nanoTime();
+			connector.writeHeader(MSIMConnector.Header.ConnectivityDetection);
+			connector.flushOutput();
+
+			// Receive link down events
+			int linkDownEventCount = connector.readInt();
+			System.out.printf(" %d:  connectivity_detection.pass = %s\n", MovementEngine.getCurrentTick(), toHumanTime(System.nanoTime() - startPass));
+			long startRecv = System.nanoTime();
+			for (int i = 0; i < linkDownEventCount; i++) {
+				int ID0 = connector.readInt();
+				int ID1 = connector.readInt();
+				NetworkInterface ni0 = interfaces.get(ID0);
+				NetworkInterface ni1 = interfaces.get(ID1);
+				ni0.linkDown(ni1);
+			}
+			System.out.printf(" %d:  connectivity_detection.recv_link_down = %s\n", MovementEngine.getCurrentTick(), toHumanTime(System.nanoTime() - startRecv));
+
+			// Receive link up events
+			int linkUpEventCount = connector.readInt();
+			long startSend = System.nanoTime();
+			for (int i = 0; i < linkUpEventCount; i++) {
+				int ID0 = connector.readInt();
+				int ID1 = connector.readInt();
+				NetworkInterface ni0 = interfaces.get(ID0);
+				NetworkInterface ni1 = interfaces.get(ID1);
+				ni0.linkUp(ni1);
+			}
+			System.out.printf(" %d:  connectivity_detection.recv_link_up = %s\n", MovementEngine.getCurrentTick(), toHumanTime(System.nanoTime() - startSend));
 		}
-	}
-
-	public void applyLinkDownEvent(int ID0, int ID1) {
-		assert(ID0 < ID1);
-		boolean success = interfacesInRange.get(ID0).remove(ID_to_NI(ID1));
-		assert(success);
-	}
-
-	public void applyLinkUpEvent(int ID0, int ID1) {
-		assert(ID0 < ID1);
-		boolean success = interfacesInRange.get(ID0).add(ID_to_NI(ID1));
-		assert(success);
-	}
-
-	// Maps a NetworkInterface to its unique ID
-	private int NI_to_ID(NetworkInterface ni) {
-		return ni.getHost().getID();
-	}
-
-	// Maps a NetworkInterfaceID to its Interface class
-	private NetworkInterface ID_to_NI(int id) {
-		return hosts.get(id).getInterfaces().get(0);
 	}
 
 }

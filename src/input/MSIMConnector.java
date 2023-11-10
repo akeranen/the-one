1e1d98d5cab6117395f013ec0c0fbe9b50111e15
crydsch <crydsch@lph.zone>
2023-11-10 19:00:05 +0100

    Add process shutdown timeout

--- src/input/MSIMConnector.java
+++ src/input/MSIMConnector.java
@@ -199,8 +199,7 @@ public class MSIMConnector {
         if (debug) return;
 
         try {
-            msim.waitFor();
-            //msim.waitFor(2, TimeUnit.SECONDS);
+            msim.waitFor(5, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             System.err.println("MSIM Process did not exit gracefully. => Killing it");
             msim.destroyForcibly();
